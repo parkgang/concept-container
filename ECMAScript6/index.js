fc8ca95f774fae984241677d0f2a1036eeb7af8f
박경은 <ruddms936@naver.com>
2020-10-04 17:38:04 +0900

    feat: 6.6

--- ECMAScript6/index.js
+++ ECMAScript6/index.js
@@ -3,3 +3,4 @@
 // 6.3 Clear: 한번 더 보기
 // 6.4 [부터 대충 봄](https://poiemaweb.com/es6-extended-parameter-handling#3-spread-%EB%AC%B8%EB%B2%95)
 // 6.5 Clear
+// 6.6 Clear: [부터 대충 봄](https://poiemaweb.com/es6-destructuring#2-%EA%B0%9D%EC%B2%B4-%EB%94%94%EC%8A%A4%ED%8A%B8%EB%9F%AD%EC%B2%98%EB%A7%81-object-destructuring)
