c5dc5ecc246d818bacdc4c9150661e919af527e4
박경은 <ruddms936@naver.com>
2020-10-07 22:18:27 +0900

    feat: Sass css 분리 config 수정

--- es6-project/webpack.config.js
+++ es6-project/webpack.config.js
@@ -1,19 +1,23 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   // entry files
   entry: ["@babel/polyfill", "./src/js/main.js", "./src/sass/main.scss"],
   // 컴파일 + 번들링된 js 파일이 저장될 경로와 이름 지정
   output: {
-    path: path.resolve(__dirname, "dist/js"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js",
   },
+  plugins: [
+    // 컴파일 + 번들링 CSS 파일이 저장될 경로와 이름 지정
+    new MiniCssExtractPlugin({ filename: "css/style.css" }),
+  ],
   module: {
     rules: [
       {
         test: /\.js$/,
         include: [path.resolve(__dirname, "src/js")],
-        exclude: /node_modules/,
         use: {
           loader: "babel-loader",
           options: {
@@ -21,14 +25,11 @@ module.exports = {
             plugins: ["@babel/plugin-proposal-class-properties"],
           },
         },
+        exclude: /node_modules/,
       },
       {
         test: /\.scss$/,
-        use: [
-          "style-loader", // creates style nodes from JS strings
-          "css-loader", // translates CSS into CommonJS
-          "sass-loader", // compiles Sass to CSS, using Node Sass by default
-        ],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
         exclude: /node_modules/,
       },
     ],
