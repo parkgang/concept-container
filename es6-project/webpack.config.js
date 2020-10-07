0f0e7dcda3de81bdd2520ca1563a42e1ce613132
박경은 <ruddms936@naver.com>
2020-10-07 21:51:44 +0900

    feat: compare용 중간커밋

---
+++ es6-project/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/js/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist/js"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, "src.js")],
+        exclude: /node_module/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
+      },
+    ],
+  },
+  devtool: "source-map",
+  mode: "development",
+};
