28ce7671dd32b4cea50b22864131fb74f09ea90b
박경은 <ruddms936@naver.com>
2020-10-08 22:43:29 +0900

    feat: file upload example 완성

--- Node.js-file-upload-example/src/js/requestHandlers.js
+++ Node.js-file-upload-example/src/js/requestHandlers.js
@@ -1,4 +1,7 @@
-const start = (response, postData) => {
+const fs = require("fs");
+const formidable = require("formidable");
+
+const start = (response) => {
   console.log("Request handler 'start' was called.");
 
   const body =
@@ -7,9 +10,9 @@ const start = (response, postData) => {
     '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' +
     "</head>" +
     "<body>" +
-    '<form action="/upload" method="post">' +
-    '<textarea name="text" rows="20" cols="60"></textarea>' +
-    '<input type="submit" value="Submit text" />' +
+    '<form action="/upload" enctype="multipart/form-data" method="post">' +
+    '<input type="file" name="upload" multiple="multiple">' +
+    '<input type="submit" value="Upload file" />' +
     "</form>" +
     "</body>" +
     "</html>";
@@ -19,13 +22,42 @@ const start = (response, postData) => {
   response.end();
 };
 
-const upload = (response, postData) => {
+const upload = (response, request) => {
   console.log("Request handler 'upload' was called.");
 
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.write(`You've sent: ${postData}`);
-  response.end();
+  const form = new formidable.IncomingForm();
+  console.log("about to parse");
+  form.parse(request, (error, fields, files) => {
+    console.log("parsing done");
+
+    // node index
+    // fs.renameSync(files.upload.path, "../../temp/test.png");
+    // npm start
+    fs.renameSync(files.upload.path, "./temp/test.png");
+
+    response.writeHead(200, { "Content-Type": "text/html" });
+    // "/show'으로 "server.start"의 onRequest 발생 route를 거쳐서 "/show"에 대한 "requestHandlers" 실행
+    response.write("<img src='/show' />");
+    response.end();
+  });
+};
+
+const show = (response) => {
+  console.log("Request handler 'show' was called.");
+  // fs.readFile("../../temp/test.png", "binary", (error, file) => {
+  fs.readFile("./temp/test.png", "binary", (error, file) => {
+    if (error) {
+      response.writeHead(500, { "Content-Type": "text/plain" });
+      response.write(error + "\n");
+      response.end();
+    } else {
+      response.writeHead(200, { "Content-Type": "image/png" });
+      response.write(file, "binary");
+      response.end();
+    }
+  });
 };
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
