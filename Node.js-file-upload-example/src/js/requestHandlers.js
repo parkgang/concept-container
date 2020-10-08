1cc2132b551ee59ec55c41c7d7b49e8311139733
박경은 <ruddms936@naver.com>
2020-10-08 21:07:04 +0900

    feat: post request

--- Node.js-file-upload-example/src/js/requestHandlers.js
+++ Node.js-file-upload-example/src/js/requestHandlers.js
@@ -1,20 +1,29 @@
-const exec = require("child_process").exec;
-
-const start = (response) => {
+const start = (response, postData) => {
   console.log("Request handler 'start' was called.");
 
-  exec("ls -lah", (error, stdout, stderr) => {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write(stdout);
-    response.end();
-  });
+  const body =
+    "<html>" +
+    "<head>" +
+    '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' +
+    "</head>" +
+    "<body>" +
+    '<form action="/upload" method="post">' +
+    '<textarea name="text" rows="20" cols="60"></textarea>' +
+    '<input type="submit" value="Submit text" />' +
+    "</form>" +
+    "</body>" +
+    "</html>";
+
+  response.writeHead(200, { "Content-Type": "text/html" });
+  response.write(body);
+  response.end();
 };
 
-const upload = (response) => {
+const upload = (response, postData) => {
   console.log("Request handler 'upload' was called.");
 
   response.writeHead(200, { "Content-Type": "text/plain" });
-  response.write("Hello Upload");
+  response.write(`You've sent: ${postData}`);
   response.end();
 };
 
--- Node.js-file-upload-example/src/js/server.js
+++ Node.js-file-upload-example/src/js/server.js
@@ -3,10 +3,20 @@ const url = require("url");
 
 const start = (route, handle) => {
   const onRequest = (request, response) => {
+    let postData = "";
     const pathname = url.parse(request.url).pathname;
     console.log(`Request for ${pathname} received.`);
 
-    route(handle, pathname, response);
+    request.setEncoding("utf8");
+
+    request.addListener("data", (postDataChunk) => {
+      postData += postDataChunk;
+      console.log(`Received POST data chunk '${postDataChunk}'.`);
+    });
+
+    request.addListener("end", () => {
+      route(handle, pathname, response, postData);
+    });
   };
 
   http.createServer(onRequest).listen(8888);
