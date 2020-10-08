898c8030b6a77aaee91caf08daf12ef55952daaa
박경은 <ruddms936@naver.com>
2020-10-08 22:43:29 +0900

    feat: file upload example 완성

--- Node.js-file-upload-example/src/js/index.js
+++ Node.js-file-upload-example/src/js/index.js
@@ -2,10 +2,13 @@ const server = require("./server");
 const router = require("./router");
 const requestHandlers = require("./requestHandlers");
 
+// requestHandlers에 함수가 추가되면 Action에 맞게 URL 맵핑을 진행해야함
 const handle = {};
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
 
 // DI
+// "$ npm start"으로 시작하면 됩니다
 server.start(router.route, handle);
--- Node.js-file-upload-example/src/js/server.js
+++ Node.js-file-upload-example/src/js/server.js
@@ -3,20 +3,9 @@ const url = require("url");
 
 const start = (route, handle) => {
   const onRequest = (request, response) => {
-    let postData = "";
     const pathname = url.parse(request.url).pathname;
     console.log(`Request for ${pathname} received.`);
-
-    request.setEncoding("utf8");
-
-    request.addListener("data", (postDataChunk) => {
-      postData += postDataChunk;
-      console.log(`Received POST data chunk '${postDataChunk}'.`);
-    });
-
-    request.addListener("end", () => {
-      route(handle, pathname, response, postData);
-    });
+    route(handle, pathname, response, request);
   };
 
   http.createServer(onRequest).listen(8888);
