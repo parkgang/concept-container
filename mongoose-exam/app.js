68b8f39e2115d78a1d867a1608f9e8cb767554d3
박경은 <ruddms936@naver.com>
2020-10-18 22:55:22 +0900

    feat: Mongoose RESTful API 구현

--- mongoose-exam/app.js
+++ mongoose-exam/app.js
@@ -1,5 +1,6 @@
+// ENV
 require("dotenv").config();
-
+// DEPENDENCIES
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -7,16 +8,23 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 4500;
 
+// Static File Service
 app.use(express.static("public"));
+// Body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Node의 native Promise 사용
 mongoose.Promise = global.Promise;
 
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, { useMongoClient: true })
   .then(() => console.log("Successfully connected to mongodb"))
   .catch((e) => console.error(e));
+
+// ROUTERS
+app.use("/todos", require("./routes/todos"));
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
