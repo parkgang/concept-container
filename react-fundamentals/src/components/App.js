19517febd7d0fbf601b601e9dc8e347a3e24bff0
박경은 <ruddms936@naver.com>
2020-10-28 03:16:51 +0900

    feat: 4-2 Clear

--- react-fundamentals/src/components/App.js
+++ react-fundamentals/src/components/App.js
@@ -7,4 +7,10 @@ class App extends React.Component {
     return <Contact />;
   }
 }
+
 export default hot(module)(App);
+
+/*
+ * 파일명과 Component Name이 같아야 디버깅에 용이하다
+ * 이벤트 처리 함수는 하위 Component의 props를 함수로 전달시켜 실행한다
+ */
