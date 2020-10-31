c8529cf6913cad32ede26d95e9c572d5f282bc11
박경은 <ruddms936@naver.com>
2020-10-31 23:32:08 +0900

    feat: 4-5 중간커밋

---
+++ react-fundamentals/src/components/ContactCreate.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class ContactCreate extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>1:00</h2>
+      </div>
+    );
+  }
+}
