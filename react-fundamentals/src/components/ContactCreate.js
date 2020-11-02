01273a7a34ffd811f91fee47097ffda1313a225b
박경은 <ruddms936@naver.com>
2020-11-02 23:51:51 +0900

    feat: 4-5 2:40

--- react-fundamentals/src/components/ContactCreate.js
+++ react-fundamentals/src/components/ContactCreate.js
@@ -1,10 +1,17 @@
 import React from "react";
 
 export default class ContactCreate extends React.Component {
+  contstructor(props) {}
+
   render() {
     return (
       <div>
-        <h2>1:00</h2>
+        <h2>Create Contact</h2>
+        <p>
+          <input type="text" name="name" placeholder="name" />
+          <input type="text" name="phone" placeholder="phone" />
+        </p>
+        <button>button</button>
       </div>
     );
   }
