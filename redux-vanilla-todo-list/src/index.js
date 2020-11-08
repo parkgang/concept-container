9c11528b8d277630a22a75ae7b54ce5bd3545429
박경은 <ruddms936@naver.com>
2020-11-08 23:20:27 +0900

    refactor: #2.4

--- redux-vanilla-todo-list/src/index.js
+++ redux-vanilla-todo-list/src/index.js
@@ -7,6 +7,7 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+// reducer 위에 주로 작성되며 오로지 object를 return 형식만 가지고 있음
 const addToDo = (text) => {
   return {
     type: ADD_TODO,
@@ -24,20 +25,27 @@ const deleteToDo = (id) => {
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      // Spread으로 배열 input 순서에 따라서 TODO List 생성 순서 변경 가능
-      return [{ text: action.text, id: Date.now() }, ...state];
+      /* Spread으로 배열 input 순서에 따라서 TODO List 생성 순서 변경 가능 */
+      // 방법1
+      // return [{ text: action.text, id: Date.now() }, ...state];
+      // 방법2
+      const newToDoObj = { text: action.text, id: Date.now() };
+      return [newToDoObj, ...state];
     case DELETE_TODO:
-      return state.filter((toDo) => toDo.id !== action.id);
+      /* array에서 object를 삭제하지 않습니다. 삭제할 object를 제외시킨 새로운 array를 만들어 반환합니다. */
+      // 방법!
+      // return state.filter((toDo) => toDo.id !== action.id);
+      // 방법2
+      const cleaned = state.filter((toDo) => toDo.id !== action.id);
+      return cleaned;
     default:
       return [];
   }
 };
 
 const store = createStore(reducer);
-store.subscribe(() => {
-  console.log(store.getState());
-});
 
+// 오로지 action을 dispatch를 하기 위한 함수
 const dispatchAddToDo = (text) => {
   store.dispatch(addToDo(text));
 };
@@ -63,6 +71,11 @@ const paintToDos = () => {
 };
 
 store.subscribe(paintToDos);
+
+// Debug
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
 const onSubmit = (e) => {
   e.preventDefault();
