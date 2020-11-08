26e9b5159658ea98a0abbc35238eeac2f4f26de7
박경은 <ruddms936@naver.com>
2020-11-09 00:40:58 +0900

    init: #3.0 Clear

---
+++ redux-react-todo-list/src/routes/Home.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+function Home() {
+  const [text, setText] = useState("");
+  function onChange(e) {
+    setText(e.target.value);
+  }
+  function onSubmit(e) {
+    e.preventDefault();
+    console.log(text);
+    setText("");
+  }
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+      <ul></ul>
+    </>
+  );
+}
+
+export default Home;
