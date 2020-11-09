1f2f4357ab61f3fb0ee65486cdf1440a93c131f5
박경은 <ruddms936@naver.com>
2020-11-09 23:03:33 +0900

    feat: 5.5 중간 커밋

--- react-ajax-tutorial/src/containers/PostContainer/PostContainer.js
+++ react-ajax-tutorial/src/containers/PostContainer/PostContainer.js
@@ -1,7 +1,42 @@
 import React, { Component } from "react";
 import { PostWrapper, Navigator, Post } from "../../components";
+import * as service from "../../services/post";
 
 export default class PostContainer extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      postId: 1,
+      // 요청이 응답을 기다리고 있는지 여부를 알려줌
+      fetching: false,
+      post: {
+        title: null,
+        body: null,
+      },
+      comments: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchPostInfo(1);
+  }
+
+  fetchPostInfo = async (postId) => {
+    // 방법1
+    // const post = await service.getPost(postId);
+    // console.log(post);
+    // const comments = await service.getComments(postId);
+    // console.log(comments);
+
+    // 방법2
+    const info = await Promise.all([
+      service.getPost(postId),
+      service.getComments(postId),
+    ]);
+
+    console.log(info);
+  };
+
   render() {
     return (
       <PostWrapper>
