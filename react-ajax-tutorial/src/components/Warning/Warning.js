b473cc0fbf18d7ef69509261fe7452144c3d0dab
박경은 <ruddms936@naver.com>
2020-11-10 00:40:29 +0900

    feat: Warning 컴포넌트 완료

--- react-ajax-tutorial/src/components/Warning/Warning.js
+++ react-ajax-tutorial/src/components/Warning/Warning.js
@@ -1,12 +1,46 @@
-import React from "react";
+import { Component } from "react";
 import "./Warning.css";
 
-const Warning = ({ message, visible }) => {
-  return (
-    <div className="Warning-wrapper">
-      <div className="Warning animated bounceIn">{message}</div>
-    </div>
-  );
-};
+class Warning extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      closing: false,
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.visible && !nextProps.visible) {
+      // visible props is changing from true -> false
+
+      this.setState({
+        closing: true,
+      });
+
+      // 1 sec after
+      setTimeout(() => {
+        this.setState({
+          closing: false,
+        });
+      }, 1000);
+    }
+  }
+
+  render() {
+    const { visible, message } = this.props;
+    const { closing } = this.state;
+
+    if (!visible && !closing) return null;
+    return (
+      <div className="Warning-wrapper">
+        <div
+          className={`Warning ${closing ? "bounceOut" : "bounceIn"} animated`}
+        >
+          {message}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Warning;
