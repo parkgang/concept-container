6bc25482fb3b67e4236d762611a60acc0e5fef89
박경은 <ruddms936@naver.com>
2020-11-09 23:55:32 +0900

    feat: 에러 처리 완료

--- react-ajax-tutorial/src/components/Navigator/Navigator.js
+++ react-ajax-tutorial/src/components/Navigator/Navigator.js
@@ -2,21 +2,25 @@ import React from "react";
 import "./Navigator.css";
 import { Button } from "semantic-ui-react";
 
-const Navigator = () => (
+const Navigator = ({ onClick, postId, disabled }) => (
   <div className="Navigator">
     <Button
       color="teal"
       content="Pervious"
       icon="left arrow"
       labelPosition="left"
+      onClick={() => onClick("PREV")}
+      disabled={disabled}
     />
-    <div className="Navigator-page-num">1</div>
+    <div className="Navigator-page-num">{postId}</div>
     <Button
       color="teal"
       content="Next"
       icon="right arrow"
       labelPosition="right"
       className="Navigator-right-button"
+      onClick={() => onClick("NEXT")}
+      disabled={disabled}
     />
   </div>
 );
