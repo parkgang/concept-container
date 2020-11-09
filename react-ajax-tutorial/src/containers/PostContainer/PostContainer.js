aa8b7ecbaa58540e4e8a1599988e03129541b49e
박경은 <ruddms936@naver.com>
2020-11-09 22:09:46 +0900

    feat: View Clear

--- react-ajax-tutorial/src/containers/PostContainer/PostContainer.js
+++ react-ajax-tutorial/src/containers/PostContainer/PostContainer.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import { PostWrapper, Navigator } from "../../components";
+import { PostWrapper, Navigator, Post } from "../../components";
 
 export default class PostContainer extends React.Component {
   render() {
     return (
       <PostWrapper>
         <Navigator />
+        <Post />
       </PostWrapper>
     );
   }
