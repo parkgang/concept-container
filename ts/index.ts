3a8ad47f93f14f755aaf3b98ab5730f54bc1def3
박경은 <63892989+ruddms936@users.noreply.github.com>
2020-12-27 05:02:43 +0900

    init

--- ts/index.ts
+++ ts/index.ts
@@ -9,3 +9,5 @@ function addPerson<T> (data: person<T>): void {
 
 addPerson<number>({id: 1, name: "박경은"});
 addPerson<string>({id: "1", name: "박경은"});
+
+// redux actionType와 같이 string 형태의 index의 경우 enum으로 처리 방법이 안전한 것으로 확인됨
