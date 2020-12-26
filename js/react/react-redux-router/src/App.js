0abf64ccacee6c60415010980655941598e7c0bb
박경은 <63892989+ruddms936@users.noreply.github.com>
2020-12-26 20:16:44 +0900

    add: ES6 이상 문법 지원

--- js/react/react-redux-router/src/App.js
+++ js/react/react-redux-router/src/App.js
@@ -19,6 +19,7 @@ export default class App extends Component {
         <Provider store={store}>
           <BrowserRouter>
             <Switch>
+              {/* host(ex: http://127.0.0.1:5500)를 제외한 해당 파일을 불러오는 경로를 작성해야 build된 파일이 react-router에 의해 렌더링 됩니다. (ex: /js/react/react-redux-router/dist) */}
               <Route exact path="/" component={DownContainer} />
               <Route exact path="/test" component={UpContainer} />
               <Route /> {/* 404 error rendering */}
