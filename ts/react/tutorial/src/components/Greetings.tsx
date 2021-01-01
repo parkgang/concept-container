fdc1321b51c510a9a2593b4b1ca36bf32424402e
박경은 <63892989+ruddms936@users.noreply.github.com>
2021-01-01 16:55:59 +0900

    edit: React.FC으로 마이그레이션

--- ts/react/tutorial/src/components/Greetings.tsx
+++ ts/react/tutorial/src/components/Greetings.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 
 interface GreetingProps {
   name: string;
-  mark: string;
+  mark?: string;
   optional?: string;
   onClick: (name: string) => void;
 }
 
-// 함수 선언식
-function Greetings({ name, mark, optional, onClick }: GreetingProps): JSX.Element {
+const Greetings: React.FC<GreetingProps> = ({ name, mark, optional, onClick }) => {
   const handleClick = () => onClick(name);
   return (
     <div>
@@ -25,28 +24,6 @@ function Greetings({ name, mark, optional, onClick }: GreetingProps): JSX.Elemen
       </div>
     </div>
   );
-}
-
-Greetings.defaultProps = {
-  mark: '!',
 };
 
 export default Greetings;
-
-// React.FC 사용 (기본값 제대로 할당되지 않는 이슈 존재)
-// const Greetings: React.FC<GreetingProps> = ({ name = '', mark = '하' }): JSX.Element => {
-//   return (
-//     <div>
-//       Hello,
-//       {name}
-//       {mark}
-//     </div>
-//   );
-// };
-
-// React.FC 함수 표현식으로 사용
-// const Greetings = ({ name, mark }: GreetingProps): JSX.Element => (
-//   <div>
-//     Hello, {name} {mark}
-//   </div>
-// );
