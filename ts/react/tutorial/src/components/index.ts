ed6ecf2a92f97772a63ae0f3d3824f48cffe2818
박경은 <63892989+ruddms936@users.noreply.github.com>
2021-01-01 23:09:54 +0900

    add: ReducerSample component

--- ts/react/tutorial/src/components/index.ts
+++ ts/react/tutorial/src/components/index.ts
@@ -2,5 +2,6 @@ import Greetings from './Greetings';
 import Counter from './Counter';
 import MyForm from './MyForm';
 import CounterReducer from './CounterReducer';
+import ReducerSample from './ReducerSample';
 
-export { Greetings, Counter, MyForm, CounterReducer };
+export { Greetings, Counter, MyForm, CounterReducer, ReducerSample };
