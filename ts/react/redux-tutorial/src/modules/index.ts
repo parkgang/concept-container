9b1b03339733a1c5f9dffb262624352313b09f8b
박경은 <63892989+ruddms936@users.noreply.github.com>
2021-01-02 19:32:26 +0900

    init: rootReducer

---
+++ ts/react/redux-tutorial/src/modules/index.ts
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+import counter from './counter';
+
+const rootReducer = combineReducers({
+  counter,
+});
+
+export default rootReducer;
+
+export type RootState = ReturnType<typeof rootReducer>;
