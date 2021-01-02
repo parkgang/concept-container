8c389d5cfdc87420c288472c89190dc9110691c9
박경은 <63892989+ruddms936@users.noreply.github.com>
2021-01-02 16:37:44 +0900

    add: Context Example

--- ts/react/context-api-tutorial/src/App.tsx
+++ ts/react/context-api-tutorial/src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 import * as components from './components';
+import { TodosContextProvider } from './contexts/TodosContext';
 
 import './App.css';
 
 function App(): JSX.Element {
   return (
-    <>
+    <TodosContextProvider>
       <components.TodoForm />
       <components.TodoList />
-    </>
+    </TodosContextProvider>
   );
 }
 
