9a25ed6005aec798782b8341e3e224157f29609d
박경은 <63892989+ruddms936@users.noreply.github.com>
2021-01-03 00:21:45 +0900

    add: 커스텀 hook connection

---
+++ ts/react/redux-tutorial/src/hooks/useAddTodo.tsx
@@ -0,0 +1,10 @@
+import { type } from 'os';
+import { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { addTodo } from '../modules/todos';
+
+export default function useAddTodo() {
+  const dispatch = useDispatch();
+  return useCallback((text) => dispatch(addTodo(text)), [dispatch]);
+}
