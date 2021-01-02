513782206e5d15c6d67ad7775f22eb3591e4859b
박경은 <63892989+ruddms936@users.noreply.github.com>
2021-01-02 19:38:26 +0900

    add: react store connection

--- ts/react/redux-tutorial/src/index.tsx
+++ ts/react/redux-tutorial/src/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import rootReducer from './modules';
+
 import App from './App';
 
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root'),
 );
