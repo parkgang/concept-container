4dadf819b876b5acb1c307f90598a09b6a27c101
박경은 <63892989+ruddms936@users.noreply.github.com>
2021-01-02 18:59:00 +0900

    add: 모듈 작성 완료

--- ts/react/redux-tutorial/src/modules/counter.ts
+++ ts/react/redux-tutorial/src/modules/counter.ts
@@ -10,3 +10,26 @@ export const increaseBy = (diff: number) => ({
 });
 
 type CounterAction = ReturnType<typeof increase> | ReturnType<typeof decrease> | ReturnType<typeof increaseBy>;
+
+type CounterState = {
+  count: number;
+};
+
+const initialState: CounterState = {
+  count: 0,
+};
+
+function counter(state: CounterState = initialState, action: CounterAction) {
+  switch (action.type) {
+    case INCREASE:
+      return { count: state.count + 1 };
+    case DECREASE:
+      return { count: state.count - 1 };
+    case INCREASE_BY:
+      return { count: state.count + action.payload };
+    default:
+      return state;
+  }
+}
+
+export default counter;
