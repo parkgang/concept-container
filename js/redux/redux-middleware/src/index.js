115a3bf3fd9959748da1e8104911f6a59ec8bfd5
박경은 <63892989+ruddms936@users.noreply.github.com>
2021-01-04 02:15:54 +0900

    add: 리덕스 미들웨어 테스트를 위한 구조 파일

--- js/redux/redux-middleware/src/index.js
+++ js/redux/redux-middleware/src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./modules";
+
 import App from "./App";
 
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
