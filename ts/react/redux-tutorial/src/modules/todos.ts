55ee7ce3438b85b40c36e5853d9d2e01540df044
박경은 <63892989+ruddms936@users.noreply.github.com>
2021-01-03 12:16:22 +0900

    refactor: 메소드 체이닝으로 변경

--- ts/react/redux-tutorial/src/modules/todos.ts
+++ ts/react/redux-tutorial/src/modules/todos.ts
@@ -1,12 +1,8 @@
 import { createAction, ActionType, createReducer } from 'typesafe-actions';
 
-const ADD_TODO = 'todos/ADD_TODO' as const;
-const TOGGLE_TODO = 'todos/TOGGLE_TODO' as const;
-const REMOVE_TODO = 'todos/REMOVE_TODO' as const;
-
-export const addTodo = createAction(ADD_TODO)<string>();
-export const toggleTodo = createAction(TOGGLE_TODO)<number>();
-export const removeTodo = createAction(REMOVE_TODO)<number>();
+export const addTodo = createAction('todos/ADD_TODO')<string>();
+export const toggleTodo = createAction('todos/TOGGLE_TODO')<number>();
+export const removeTodo = createAction('todos/REMOVE_TODO')<number>();
 
 const actions = { addTodo, toggleTodo, removeTodo };
 type TodosAction = ActionType<typeof actions>;
@@ -25,16 +21,17 @@ const initialState: TodosState = [
   { id: 3, text: '투두리스트 만들기', done: false },
 ];
 
-const todos = createReducer<TodosState, TodosAction>(initialState, {
-  [ADD_TODO]: (state, { payload: text }) =>
+const todos = createReducer<TodosState, TodosAction>(initialState)
+  .handleAction(addTodo, (state, { payload: text }) =>
     state.concat({
       id: Math.max(...state.map((todo) => todo.id)) + 1,
       text,
       done: false,
     }),
-  [TOGGLE_TODO]: (state, { payload: id }) =>
+  )
+  .handleAction(toggleTodo, (state, { payload: id }) =>
     state.map((todo) => (todo.id === id ? { ...todo, done: !todo.done } : todo)),
-  [REMOVE_TODO]: (state, { payload: id }) => state.filter((todo) => todo.id !== id),
-});
+  )
+  .handleAction(removeTodo, (state, { payload: id }) => state.filter((todo) => todo.id !== id));
 
 export default todos;
