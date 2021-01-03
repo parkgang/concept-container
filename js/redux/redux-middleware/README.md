6985a7ebece29143dd327a38223ca63e6b6f75a6
박경은 <63892989+ruddms936@users.noreply.github.com>
2021-01-04 02:08:30 +0900

    init

---
+++ js/redux/redux-middleware/README.md
@@ -0,0 +1,3 @@
+# Hi
+
+redux-middleware를 연습하는 프로젝트입니다.
---
+++ js/redux/redux-middleware/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <span>Hello, world</span>
+    </div>
+  );
+}
+
+export default App;
