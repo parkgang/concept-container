23d4d1ae7bc0e2e530e3197f4fcc8b9a73e26453
박경은 <63892989+ruddms936@users.noreply.github.com>
2021-01-04 02:46:52 +0900

    add: 새로운 상태값 확인하도록 미들웨어 수정

--- js/react/redux-middleware/src/middlewares/myLogger.js
+++ js/react/redux-middleware/src/middlewares/myLogger.js
@@ -3,6 +3,8 @@ const myLogger = (store) => (next) => (action) => {
   console.log(action);
   // 다음 미들웨어 (또는 리듀서) 에게 액션을 전달합니다.
   const result = next(action);
+  // 업데이트 이후의 상태를 조회합니다. ('\t' 는 탭 문자 입니다.)
+  console.log("\t", store.getState());
   // 여기서 반환하는 값은 dispatch(action)의 결과물이 됩니다. 기본: undefined
   return result;
 };
