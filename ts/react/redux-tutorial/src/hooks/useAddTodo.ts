a3d45b74b427a1ee13e3feab0652ef8b9505bc3c
박경은 <63892989+ruddms936@users.noreply.github.com>
2021-01-03 17:29:35 +0900

    edit: return type를 지정한 샘플 코드

--- ts/react/redux-tutorial/src/hooks/useAddTodo.ts
+++ ts/react/redux-tutorial/src/hooks/useAddTodo.ts
@@ -1,10 +1,12 @@
-import { type } from 'os';
 import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 
 import { addTodo } from '../modules/todos';
 
-export default function useAddTodo() {
+// (text: string) => ReturnType<typeof addTodo>
+// (text: string) => PayloadAction<'todos/ADD_TODO', string>
+
+export default function useAddTodo(): (text: string) => ReturnType<typeof addTodo> {
   const dispatch = useDispatch();
   return useCallback((text) => dispatch(addTodo(text)), [dispatch]);
 }
