931788a937d0b6c2b37b1bfcc04f657b900e35de
박경은 <63892989+ruddms936@users.noreply.github.com>
2021-01-04 02:15:54 +0900

    add: 리덕스 미들웨어 테스트를 위한 구조 파일

--- js/redux/redux-middleware/src/App.js
+++ js/redux/redux-middleware/src/App.js
@@ -1,10 +1,14 @@
+import React from "react";
+
+import CounterContainer from "./containers/CounterContainer";
+
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <span>Hello, world</span>
-    </div>
+    <>
+      <CounterContainer />
+    </>
   );
 }
 
