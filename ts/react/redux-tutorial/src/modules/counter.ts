fbb776488f23c29a59eee6b9bc5fa1aedd73f754
박경은 <63892989+ruddms936@users.noreply.github.com>
2021-01-03 11:29:40 +0900

    refactor: typesafe-actions 메소드 체이닝으로 변경

--- ts/react/redux-tutorial/src/modules/counter.ts
+++ ts/react/redux-tutorial/src/modules/counter.ts
@@ -1,15 +1,11 @@
 import { createAction, ActionType, createReducer } from 'typesafe-actions';
 
-const INCREASE = 'counter/INCREASE';
-const DECREASE = 'counter/DECREASE';
-const INCREASE_BY = 'counter/INCREASE_BY';
-
 // () => ({ type: INCREASE })
-export const increase = createAction(INCREASE)();
+export const increase = createAction('counter/INCREASE')();
 // () => ({ type: DECREASE })
-export const decrease = createAction(DECREASE)();
+export const decrease = createAction('counter/DECREASE')();
 // (payload: number) => ({ type: INCREASE_BY, payload })
-export const increaseBy = createAction(INCREASE_BY)<number>();
+export const increaseBy = createAction('counter/INCREASE_BY')<number>();
 
 // 유니온 작업이 필요한 action 생성 함수 타입을 한번에 생성해줌
 const actions = { increase, decrease, increaseBy };
@@ -24,10 +20,11 @@ const initialState: CounterState = {
 };
 
 // 리듀서
-const counter = createReducer<CounterState, CounterAction>(initialState, {
-  [INCREASE]: (state) => ({ count: state.count + 1 }),
-  [DECREASE]: (state) => ({ count: state.count - 1 }),
-  [INCREASE_BY]: (state, action) => ({ count: state.count + action.payload }),
-});
+const counter = createReducer<CounterState, CounterAction>(initialState)
+  .handleAction(increase, (state) => ({ count: state.count + 1 }))
+  .handleAction(decrease, (state) => ({ count: state.count - 1 }))
+  .handleAction(increaseBy, (state, action) => ({
+    count: state.count + action.payload,
+  }));
 
 export default counter;
