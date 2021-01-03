d1e9601a8d968db5b31bab4b3df9983b0d9caf0f
박경은 <63892989+ruddms936@users.noreply.github.com>
2021-01-04 00:36:54 +0900

    up: Redux useSelector 최적화

--- ts/react/redux-tutorial/src/hooks/useTodos.ts
+++ ts/react/redux-tutorial/src/hooks/useTodos.ts
@@ -1,9 +1,9 @@
-import { useSelector } from 'react-redux';
+import { useSelector, shallowEqual } from 'react-redux';
 
 import { RootState } from '../modules';
 import { TodosState } from '../modules/todos';
 
 export default function useTodos(): TodosState {
-  const todos = useSelector((state: RootState) => state.todos);
+  const todos = useSelector((state: RootState) => state.todos, shallowEqual);
   return todos;
 }
