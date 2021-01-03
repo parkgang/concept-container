3fbf2c65e3168119d31141dd28d4fd9ebf756690
박경은 <63892989+ruddms936@users.noreply.github.com>
2021-01-03 18:08:17 +0900

    edit: 액션 생성 함수 반환 타입 지정 docs: 의존성 수정 내용 추가

--- ts/react/redux-tutorial/src/modules/vanilla-react-redux/counter.ts
+++ ts/react/redux-tutorial/src/modules/vanilla-react-redux/counter.ts
@@ -2,9 +2,14 @@ const INCREASE = 'counter/INCREASE' as const;
 const DECREASE = 'counter/DECREASE' as const;
 const INCREASE_BY = 'counter/INCREASE_BY' as const;
 
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
-export const increaseBy = (diff: number) => ({
+interface ActionCreateFun<T> {
+  type: string;
+  payload?: T;
+}
+
+export const increase = (): ActionCreateFun<null> => ({ type: INCREASE });
+export const decrease = (): ActionCreateFun<null> => ({ type: DECREASE });
+export const increaseBy = (diff: number): ActionCreateFun<number> => ({
   type: INCREASE_BY,
   payload: diff,
 });
@@ -19,14 +24,14 @@ const initialState: CounterState = {
   count: 0,
 };
 
-function counter(state: CounterState = initialState, action: CounterAction) {
+function counter(state: CounterState = initialState, action: CounterAction): CounterState {
   switch (action.type) {
     case INCREASE:
       return { count: state.count + 1 };
     case DECREASE:
       return { count: state.count - 1 };
     case INCREASE_BY:
-      return { count: state.count + action.payload };
+      return { count: state.count + (action.payload as number) };
     default:
       return state;
   }
