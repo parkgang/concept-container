480a20d85b852bb3c7dbba03b39aa239f1098144
박경은 <63892989+ruddms936@users.noreply.github.com>
2021-01-05 08:00:11 +0900

    addL redux-thunk 적용

--- js/react/redux-middleware/redux-thunk/src/containers/CounterContainer.js
+++ js/react/redux-middleware/redux-thunk/src/containers/CounterContainer.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import Counter from '../components/Counter';
 import { useSelector, useDispatch } from 'react-redux';
-import { increase, decrease } from '../modules/counter';
+
+import Counter from '../components/Counter';
+import { increaseAsync, decreaseAsync } from '../modules/counter';
 
 function CounterContainer() {
   const number = useSelector((state) => state.counter);
   const dispatch = useDispatch();
 
   const onIncrease = () => {
-    dispatch(increase());
+    dispatch(increaseAsync());
   };
   const onDecrease = () => {
-    dispatch(decrease());
+    dispatch(decreaseAsync());
   };
 
   return <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />;
