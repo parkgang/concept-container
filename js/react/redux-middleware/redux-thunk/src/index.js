64217df6b0b54e6305592b4f7574c17dfc4675d5
박경은 <63892989+ruddms936@users.noreply.github.com>
2021-01-05 06:49:34 +0900

    init: redux-thunk

---
+++ js/react/redux-middleware/redux-thunk/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import rootReducer from './modules';
+// "applyMiddleware"에 여러개의 미들웨어를 적용 할 수 있습니다.
+import myLogger from './middlewares/myLogger';
+
+import App from './App';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(myLogger)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
