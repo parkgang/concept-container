59aafa69dc60a8896f68bd44d136a32a26226149
Kyungeun <ruddms936@naver.com>
2021-01-10 23:41:46 +0900

    add: react-router-dom 적용

--- js/react/redux-middleware/redux-thunk/src/index.js
+++ js/react/redux-middleware/redux-thunk/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
@@ -9,6 +10,7 @@ import ReduxThunk from 'redux-thunk';
 import rootReducer from './modules';
 
 import App from './App';
+
 const store = createStore(
   rootReducer,
   // logger 를 사용하는 경우, logger가 가장 마지막에 와야합니다.
@@ -16,8 +18,10 @@ const store = createStore(
 );
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root'),
 );
