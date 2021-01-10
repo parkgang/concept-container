d279b1805f27e35d10aacd4ff68bf8956e9f2503
Kyungeun <ruddms936@naver.com>
2021-01-10 23:41:46 +0900

    add: react-router-dom 적용

--- js/react/redux-middleware/redux-thunk/src/App.js
+++ js/react/redux-middleware/redux-thunk/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
-import CounterContainer from './containers/CounterContainer';
-import PostListContainer from './containers/PostListContainer';
+import PostListPage from './pages/PostListPage';
+import PostPage from './pages/PostPage';
 
 import './App.css';
-
-const components = { CounterContainer, PostListContainer };
 
 function App() {
   return (
     <>
-      <components.PostListContainer />
+      <Route path="/" component={PostListPage} exact={true} />
+      <Route path="/:id" component={PostPage} />
     </>
   );
 }
