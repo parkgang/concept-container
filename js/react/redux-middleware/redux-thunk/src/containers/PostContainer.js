b7d12c2ae0b86dc70a4b3cbd2539c5b4dc467851
Kyungeun <ruddms936@naver.com>
2021-01-12 01:55:36 +0900

    up: id에 data가 있으면 요청하지 않는 방식

--- js/react/redux-middleware/redux-thunk/src/containers/PostContainer.js
+++ js/react/redux-middleware/redux-thunk/src/containers/PostContainer.js
@@ -1,19 +1,21 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { getPost, clearPost } from '../modules/posts';
+import { getPost } from '../modules/posts';
 import Post from '../components/Post';
 
 function PostContainer({ postId }) {
-  const { data, loading, error } = useSelector((state) => state.posts.post);
+  const { data, loading, error } = useSelector((state) => state.posts.post[postId]) || {
+    loading: false,
+    data: null,
+    error: null,
+  }; // 아예 데이터가 존재하지 않을 때가 있으므로, 비구조화 할당이 오류나지 않도록
   const dispatch = useDispatch();
 
   useEffect(() => {
+    if (data) return; // 포스트가 존재하면 아예 요청을 하지 않음
     dispatch(getPost(postId));
-    return () => {
-      dispatch(clearPost());
-    };
-  }, [postId, dispatch]);
+  }, [postId, dispatch, data]);
 
   if (loading) return <div>로딩중...</div>;
   if (error) return <div>에러 발생!</div>;
