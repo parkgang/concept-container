25a7ac305bf35e4a50e959048ed9ca1c561584df
Kyungeun <ruddms936@naver.com>
2021-01-11 23:55:05 +0900

    up: 캐싱된 데이터 존재하면 요청 안함

--- js/react/redux-middleware/redux-thunk/src/containers/PostListContainer.js
+++ js/react/redux-middleware/redux-thunk/src/containers/PostListContainer.js
@@ -10,8 +10,9 @@ function PostListContainer() {
 
   // 컴포넌트 마운트 후 포스트 목록 요청
   useEffect(() => {
+    if (data) return;
     dispatch(getPosts());
-  }, [dispatch]);
+  }, [data, dispatch]);
 
   if (loading) return <div>로딩중...</div>;
   if (error) return <div>에러 발생!</div>;
