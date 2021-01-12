6210fb55eb101dd15d0e6ac97cfb38616bc6a55a
Kyungeun <ruddms936@naver.com>
2021-01-12 23:41:42 +0900

    up: redux-thunk 내부에서 react-router 사용 예제

--- js/react/redux-middleware/redux-thunk/src/containers/PostContainer.js
+++ js/react/redux-middleware/redux-thunk/src/containers/PostContainer.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { getPost } from '../modules/posts';
+import { getPost, goToHome } from '../modules/posts';
 import Post from '../components/Post';
 
 function PostContainer({ postId }) {
@@ -20,7 +20,12 @@ function PostContainer({ postId }) {
   if (error) return <div>에러 발생!</div>;
   if (!data) return null;
 
-  return <Post post={data} />;
+  return (
+    <>
+      <button onClick={() => dispatch(goToHome())}>홈으로 이동</button>
+      <Post post={data} />
+    </>
+  );
 }
 
 export default PostContainer;
