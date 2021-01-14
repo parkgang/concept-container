533d3149c13ae694b6ee4d41a68558828279e983
Kyungeun <ruddms936@naver.com>
2021-01-15 02:35:33 +0900

    add: 복습 차원 연습장

--- js/es6/study/promise.js
+++ js/es6/study/promise.js
@@ -1,3 +1,23 @@
-(async (value) => {
-  console.log(value);
-})("박경은");
+// 추후 보기 위해 작성한 코드가 아닙니다. 연습장
+let main = async () => {
+  let task1 = () =>
+    new Promise((resolve, reject) => {
+      resolve(10);
+    })
+      .then((value) => {
+        console.log(`task1 close: ${value}`);
+        throw new Error("에러 만들기");
+      })
+      .catch((e) => {
+        console.error(`task1 오류: ${e}`);
+        throw new Error(e);
+      });
+
+  try {
+    await task1();
+  } catch (error) {
+    console.error(`main 오류: ${error}`);
+  }
+};
+
+main();
