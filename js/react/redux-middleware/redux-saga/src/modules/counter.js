c1864acfeeb31749e558cefcd44d35e99e2e1131
Kyungeun <ruddms936@naver.com>
2021-01-16 08:21:42 +0900

    add: saga function

--- js/react/redux-middleware/redux-saga/src/modules/counter.js
+++ js/react/redux-middleware/redux-saga/src/modules/counter.js
@@ -1,3 +1,5 @@
+import { delay, put } from 'redux-saga/effects';
+
 // 액션 타입
 const INCREASE = 'INCREASE';
 const DECREASE = 'DECREASE';
@@ -9,6 +11,15 @@ export const increase = () => ({ type: INCREASE });
 export const decrease = () => ({ type: DECREASE });
 export const increaseAsync = () => ({ type: INCREASE_ASYNC });
 export const decreaseAsync = () => ({ type: DECREASE_ASYNC });
+
+function* increaseSaga() {
+  yield delay(1000); // 1초를 기다립니다.
+  yield put(increase()); // put은 특정 액션을 디스패치 해줍니다.
+}
+function* decreaseSaga() {
+  yield delay(1000); // 1초를 기다립니다.
+  yield put(decrease()); // put은 특정 액션을 디스패치 해줍니다.
+}
 
 // 초깃값 (상태가 객체가 아니라 그냥 숫자여도 상관 없습니다.)
 const initialState = 0;
