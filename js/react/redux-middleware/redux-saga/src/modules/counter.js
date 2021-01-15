7ac8135b9cb2dc9c49b439c6c6a09792e948e13b
Kyungeun <ruddms936@naver.com>
2021-01-16 08:12:50 +0900

    up: saga으로 교체

--- js/react/redux-middleware/redux-saga/src/modules/counter.js
+++ js/react/redux-middleware/redux-saga/src/modules/counter.js
@@ -1,16 +1,16 @@
+// 액션 타입
 const INCREASE = 'INCREASE';
 const DECREASE = 'DECREASE';
+const INCREASE_ASYNC = 'INCREASE_ASYNC';
+const DECREASE_ASYNC = 'DECREASE_ASYNC';
 
+// 액션 생성 함수
 export const increase = () => ({ type: INCREASE });
 export const decrease = () => ({ type: DECREASE });
+export const increaseAsync = () => ({ type: INCREASE_ASYNC });
+export const decreaseAsync = () => ({ type: DECREASE_ASYNC });
 
-export const increaseAsync = () => (dispatch) => {
-  setTimeout(() => dispatch(increase()), 1000);
-};
-export const decreaseAsync = () => (dispatch) => {
-  setTimeout(() => dispatch(decrease()), 1000);
-};
-
+// 초깃값 (상태가 객체가 아니라 그냥 숫자여도 상관 없습니다.)
 const initialState = 0;
 
 export default function counter(state = initialState, action) {
