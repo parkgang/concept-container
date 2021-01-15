b42dd440fcb6429acc156e2b4acc12f3dd14b41e
Kyungeun <ruddms936@naver.com>
2021-01-16 08:45:34 +0900

    use: sagaMiddleware

--- js/react/redux-middleware/redux-saga/src/index.js
+++ js/react/redux-middleware/redux-saga/src/index.js
@@ -6,19 +6,30 @@ import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 import { createBrowserHistory } from 'history';
 
-import rootReducer from './modules';
+import rootReducer, { rootSaga } from './modules';
 
 import App from './App';
 
 const customHistory = createBrowserHistory();
+const sagaMiddleware = createSagaMiddleware(); // 사가 미들웨어를 만듭니다.
 
 const store = createStore(
   rootReducer,
   // logger 를 사용하는 경우, logger가 가장 마지막에 와야합니다.
-  composeWithDevTools(applyMiddleware(ReduxThunk.withExtraArgument({ history: customHistory }), logger)),
+  composeWithDevTools(
+    applyMiddleware(
+      ReduxThunk.withExtraArgument({ history: customHistory }),
+      sagaMiddleware, // 사가 미들웨어를 적용하고
+      logger,
+    ),
+  ),
 ); // 여러개의 미들웨어를 적용 할 수 있습니다.
+
+sagaMiddleware.run(rootSaga); // 루트 사가를 실행해줍니다.
+// 주의: 스토어 생성이 된 다음에 위 코드를 실행해야합니다.
 
 ReactDOM.render(
   <Router history={customHistory}>
