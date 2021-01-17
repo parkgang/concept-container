2b3d22c5fabea9400fdf456b36bfabe7df24c9c4
Kyungeun <ruddms936@naver.com>
2021-01-17 21:20:35 +0900

    up: post부분 saga으로 변경

--- js/react/redux-middleware/redux-saga/src/modules/index.js
+++ js/react/redux-middleware/redux-saga/src/modules/index.js
@@ -2,12 +2,12 @@ import { combineReducers } from 'redux';
 import { all } from 'redux-saga/effects';
 
 import counter, { counterSaga } from './counter';
-import posts from './posts';
+import posts, { postsSaga } from './posts';
 
 const rootReducer = combineReducers({ counter, posts });
 
 export function* rootSaga() {
-  yield all([counterSaga()]); // all 은 배열 안의 여러 사가를 동시에 실행시켜줍니다.
+  yield all([counterSaga(), postsSaga()]); // all 은 배열 안의 여러 사가를 동시에 실행시켜줍니다.
 }
 
 export default rootReducer;
