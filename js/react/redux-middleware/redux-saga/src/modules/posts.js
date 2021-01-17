ecff6f72667da311f39673052ca86ff05efb8e6c
Kyungeun <ruddms936@naver.com>
2021-01-17 21:20:35 +0900

    up: post부분 saga으로 변경

--- js/react/redux-middleware/redux-saga/src/modules/posts.js
+++ js/react/redux-middleware/redux-saga/src/modules/posts.js
@@ -1,11 +1,7 @@
+import { call, put, takeEvery } from 'redux-saga/effects';
+
 import * as postsAPI from '../api/posts'; // api/posts 안의 함수 모두 불러오기
-import {
-  createPromiseThunk,
-  reducerUtils,
-  handleAsyncActions,
-  createPromiseThunkById,
-  handleAsyncActionsById,
-} from '../lib/asyncUtils';
+import { reducerUtils, handleAsyncActions, handleAsyncActionsById } from '../lib/asyncUtils';
 
 /* 액션 타입 */
 
@@ -19,14 +15,57 @@ const GET_POST = 'GET_POST';
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_ERROR = 'GET_POST_ERROR';
 
-// 아주 쉽게 thunk 함수를 만들 수 있게 되었습니다.
-export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = createPromiseThunkById(GET_POST, postsAPI.getPostById);
+export const getPosts = () => ({ type: GET_POSTS });
+// payload는 파라미터 용도, meta는 리듀서에서 id를 알기위한 용도
+export const getPost = (id) => ({ type: GET_POST, payload: id, meta: id });
+
+function* getPostsSaga() {
+  try {
+    const posts = yield call(postsAPI.getPosts); // call 을 사용하면 특정 함수를 호출하고, 결과물이 반환 될 때까지 기다려줄 수 있습니다.
+    yield put({
+      type: GET_POSTS_SUCCESS,
+      payload: posts,
+    }); // 성공 액션 디스패치
+  } catch (e) {
+    yield put({
+      type: GET_POSTS_ERROR,
+      error: true,
+      payload: e,
+    }); // 실패 액션 디스패치
+  }
+}
+
+// 액션이 지니고 있는 값을 조회하고 싶다면 action을 파라미터로 받아와서 사용 할 수 있습니다.
+function* getPostSaga(action) {
+  const param = action.payload;
+  const id = action.meta;
+  try {
+    const post = yield call(postsAPI.getPostById, param); // API 함수에 넣어주고 싶은 인자는 call 함수의 두번째 인자부터 순서대로 넣어주면 됩니다.
+    yield put({
+      type: GET_POST_SUCCESS,
+      payload: post,
+      meta: id,
+    });
+  } catch (e) {
+    yield put({
+      type: GET_POST_ERROR,
+      error: true,
+      payload: e,
+      meta: id,
+    });
+  }
+}
+
+// 사가들을 합치기
+export function* postsSaga() {
+  yield takeEvery(GET_POSTS, getPostsSaga);
+  yield takeEvery(GET_POST, getPostSaga);
+}
 
 // initialState 쪽도 반복되는 코드를 initial() 함수를 사용해서 리팩토링 했습니다.
 const initialState = {
   posts: reducerUtils.initial(),
-  post: {},
+  post: reducerUtils.initial(),
 };
 
 export default function posts(state = initialState, action) {
