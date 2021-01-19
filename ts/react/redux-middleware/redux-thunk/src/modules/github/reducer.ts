835c36c6b221f698cab67e7553d87b8cb7b9c8a4
Kyungeun <ruddms936@naver.com>
2021-01-20 01:29:19 +0900

    refactor: type import

--- ts/react/redux-middleware/redux-thunk/src/modules/github/reducer.ts
+++ ts/react/redux-middleware/redux-thunk/src/modules/github/reducer.ts
@@ -1,7 +1,7 @@
 import { createReducer } from 'typesafe-actions';
 import { GithubState, GithubAction } from './types';
-import { GET_USER_PROFILE, GET_USER_PROFILE_SUCCESS, GET_USER_PROFILE_ERROR } from './actions';
-import { asyncState } from '../../lib/reducerUtils';
+import { getUserProfileAsync, GET_USER_PROFILE, GET_USER_PROFILE_SUCCESS, GET_USER_PROFILE_ERROR } from './actions';
+import { asyncState, createAsyncReducer } from '../../lib/reducerUtils';
 
 // const initialState: GithubState = {
 //   userProfile: {
@@ -15,6 +15,7 @@ const initialState: GithubState = {
   userProfile: asyncState.initial(),
 };
 
+// refactor: 1
 // const github = createReducer<GithubState, GithubAction>(initialState, {
 //   [GET_USER_PROFILE]: (state) => ({
 //     ...state,
@@ -42,19 +43,25 @@ const initialState: GithubState = {
 //   }),
 // });
 
-const github = createReducer<GithubState, GithubAction>(initialState, {
-  [GET_USER_PROFILE]: (state) => ({
-    ...state,
-    userProfile: asyncState.load(),
-  }),
-  [GET_USER_PROFILE_SUCCESS]: (state, action) => ({
-    ...state,
-    userProfile: asyncState.success(action.payload),
-  }),
-  [GET_USER_PROFILE_ERROR]: (state, action) => ({
-    ...state,
-    userProfile: asyncState.error(action.payload),
-  }),
-});
+// refactor: 2
+// const github = createReducer<GithubState, GithubAction>(initialState, {
+//   [GET_USER_PROFILE]: (state) => ({
+//     ...state,
+//     userProfile: asyncState.load(),
+//   }),
+//   [GET_USER_PROFILE_SUCCESS]: (state, action) => ({
+//     ...state,
+//     userProfile: asyncState.success(action.payload),
+//   }),
+//   [GET_USER_PROFILE_ERROR]: (state, action) => ({
+//     ...state,
+//     userProfile: asyncState.error(action.payload),
+//   }),
+// });
+
+const github = createReducer<GithubState, GithubAction>(initialState).handleAction(
+  [getUserProfileAsync.request, getUserProfileAsync.success, getUserProfileAsync.failure],
+  createAsyncReducer(getUserProfileAsync, 'userProfile'),
+);
 
 export default github;
