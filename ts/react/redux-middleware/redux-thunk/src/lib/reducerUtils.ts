69fa6a3e5a48cec5f2ff3810a036d2c1c04a368d
Kyungeun <ruddms936@naver.com>
2021-01-20 01:16:25 +0900

    refactor: 모듈화

--- ts/react/redux-middleware/redux-thunk/src/lib/reducerUtils.ts
+++ ts/react/redux-middleware/redux-thunk/src/lib/reducerUtils.ts
@@ -1,3 +1,6 @@
+import { AnyAction } from 'redux';
+import { getType, AsyncActionCreatorBuilder } from 'typesafe-actions';
+
 export type AsyncState<T, E = any> = {
   data: T | null;
   loading: boolean;
@@ -27,3 +30,38 @@ export const asyncState = {
     error: error,
   }),
 };
+
+type AnyAsyncActionCreator = AsyncActionCreatorBuilder<any, any, any>;
+
+export function createAsyncReducer<S, AC extends AnyAsyncActionCreator, K extends keyof S>(
+  asyncActionCreator: AC,
+  key: K,
+) {
+  return (state: S, action: AnyAction) => {
+    // 각 액션 생성함수의 type 을 추출해줍니다.
+    const [request, success, failure] = [
+      asyncActionCreator.request,
+      asyncActionCreator.success,
+      asyncActionCreator.failure,
+    ].map(getType);
+    switch (action.type) {
+      case request:
+        return {
+          ...state,
+          [key]: asyncState.load(),
+        };
+      case success:
+        return {
+          ...state,
+          [key]: asyncState.success(action.payload),
+        };
+      case failure:
+        return {
+          ...state,
+          [key]: asyncState.error(action.payload),
+        };
+      default:
+        return state;
+    }
+  };
+}
