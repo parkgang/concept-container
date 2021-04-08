5d6ae244ac7b0685154cc8877baefce0d4bd8cff
박경은 <ruddms936@naver.com>
2021-04-08 21:42:16 +0900

    refactor: 파일 module 화

--- recoil/js/official-basic-tutorial/src/App.js
+++ recoil/js/official-basic-tutorial/src/App.js
@@ -1,15 +1,14 @@
-import { useState } from "react";
-import { atom, selector, useRecoilValue, useSetRecoilState, useRecoilState } from "recoil";
+import { useRecoilValue } from "recoil";
 
-const todoListState = atom({
-  key: "todoListState",
-  default: [],
-});
+import TodoListStats from "./components/TodoListStats";
+import TodoListFilters from "./components/TodoListFilters";
+import TodoItemCreator from "./components/TodoItemCreator";
+import TodoItem from "./components/TodoItem";
+import { filteredTodoListState } from "./state";
 
-function TodoList() {
-  // changed from todoListState to filteredTodoListState
+function App() {
+  // todoListState에서 filteredTodoListState로 변경됨
   const todoList = useRecoilValue(filteredTodoListState);
-
   return (
     <>
       <TodoListStats />
@@ -19,167 +18,6 @@ function TodoList() {
       {todoList.map((todoItem) => (
         <TodoItem item={todoItem} key={todoItem.id} />
       ))}
-    </>
-  );
-}
-
-function TodoItemCreator() {
-  const [inputValue, setInputValue] = useState("");
-  const setTodoList = useSetRecoilState(todoListState);
-
-  const addItem = () => {
-    setTodoList((oldTodoList) => [
-      ...oldTodoList,
-      {
-        id: getId(),
-        text: inputValue,
-        isComplete: false,
-      },
-    ]);
-    setInputValue("");
-  };
-
-  const onChange = ({ target: { value } }) => {
-    setInputValue(value);
-  };
-
-  return (
-    <div>
-      <input type="text" value={inputValue} onChange={onChange} />
-      <button onClick={addItem}>Add</button>
-    </div>
-  );
-}
-
-// utility for creating unique Id
-let id = 0;
-function getId() {
-  return id++;
-}
-
-function TodoItem({ item }) {
-  const [todoList, setTodoList] = useRecoilState(todoListState);
-  const index = todoList.findIndex((listItem) => listItem === item);
-
-  const editItemText = ({ target: { value } }) => {
-    const newList = replaceItemAtIndex(todoList, index, {
-      ...item,
-      text: value,
-    });
-
-    setTodoList(newList);
-  };
-
-  const toggleItemCompletion = () => {
-    const newList = replaceItemAtIndex(todoList, index, {
-      ...item,
-      isComplete: !item.isComplete,
-    });
-
-    setTodoList(newList);
-  };
-
-  const deleteItem = () => {
-    const newList = removeItemAtIndex(todoList, index);
-
-    setTodoList(newList);
-  };
-
-  return (
-    <div>
-      <input type="text" value={item.text} onChange={editItemText} />
-      <input type="checkbox" checked={item.isComplete} onChange={toggleItemCompletion} />
-      <button onClick={deleteItem}>X</button>
-    </div>
-  );
-}
-
-function replaceItemAtIndex(arr, index, newValue) {
-  return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
-}
-
-function removeItemAtIndex(arr, index) {
-  return [...arr.slice(0, index), ...arr.slice(index + 1)];
-}
-
-const todoListFilterState = atom({
-  key: "todoListFilterState",
-  default: "Show All",
-});
-
-const filteredTodoListState = selector({
-  key: "filteredTodoListState",
-  get: ({ get }) => {
-    const filter = get(todoListFilterState);
-    const list = get(todoListState);
-
-    switch (filter) {
-      case "Show Completed":
-        return list.filter((item) => item.isComplete);
-      case "Show Uncompleted":
-        return list.filter((item) => !item.isComplete);
-      default:
-        return list;
-    }
-  },
-});
-
-function TodoListFilters() {
-  const [filter, setFilter] = useRecoilState(todoListFilterState);
-
-  const updateFilter = ({ target: { value } }) => {
-    setFilter(value);
-  };
-
-  return (
-    <>
-      Filter:
-      <select value={filter} onChange={updateFilter}>
-        <option value="Show All">All</option>
-        <option value="Show Completed">Completed</option>
-        <option value="Show Uncompleted">Uncompleted</option>
-      </select>
-    </>
-  );
-}
-
-const todoListStatsState = selector({
-  key: "todoListStatsState",
-  get: ({ get }) => {
-    const todoList = get(todoListState);
-    const totalNum = todoList.length;
-    const totalCompletedNum = todoList.filter((item) => item.isComplete).length;
-    const totalUncompletedNum = totalNum - totalCompletedNum;
-    const percentCompleted = totalNum === 0 ? 0 : (totalCompletedNum / totalNum) * 100;
-
-    return {
-      totalNum,
-      totalCompletedNum,
-      totalUncompletedNum,
-      percentCompleted,
-    };
-  },
-});
-
-function TodoListStats() {
-  const { totalNum, totalCompletedNum, totalUncompletedNum, percentCompleted } = useRecoilValue(todoListStatsState);
-
-  const formattedPercentCompleted = Math.round(percentCompleted);
-
-  return (
-    <ul>
-      <li>Total items: {totalNum}</li>
-      <li>Items completed: {totalCompletedNum}</li>
-      <li>Items not completed: {totalUncompletedNum}</li>
-      <li>Percent completed: {formattedPercentCompleted}</li>
-    </ul>
-  );
-}
-
-function App() {
-  return (
-    <>
-      <TodoList />
     </>
   );
 }
---
+++ recoil/js/official-basic-tutorial/src/components/TodoItemCreator.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { useSetRecoilState } from "recoil";
+
+import { todoListState } from "../state";
+
+// 고유 ID를 만드는 유틸리티
+let id = 0;
+function getId() {
+  return id++;
+}
+
+function TodoItemCreator() {
+  const [inputValue, setInputValue] = useState("");
+  const setTodoList = useSetRecoilState(todoListState);
+
+  const addItem = () => {
+    setTodoList((oldTodoList) => [
+      ...oldTodoList,
+      {
+        id: getId(),
+        text: inputValue,
+        isComplete: false,
+      },
+    ]);
+    setInputValue("");
+  };
+
+  const onChange = ({ target: { value } }) => {
+    setInputValue(value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={inputValue} onChange={onChange} />
+      <button onClick={addItem}>Add</button>
+    </div>
+  );
+}
+
+export default TodoItemCreator;
