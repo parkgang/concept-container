0e2140d7d59234bceeeddf0de9bcce2236e49fa1
박경은 <ruddms936@naver.com>
2021-04-08 11:33:57 +0900

    use: recoil

--- recoil-helloworld/src/App.js
+++ recoil-helloworld/src/App.js
@@ -1,10 +1,56 @@
-import "./App.css";
+import React from "react";
+import { RecoilRoot, atom, selector, useRecoilState, useRecoilValue } from "recoil";
+
+const textState = atom({
+  key: "textState", // unique ID (with respect to other atoms/selectors)
+  default: "", // default value (aka initial value)
+});
+
+function CharacterCounter() {
+  return (
+    <div>
+      <TextInput />
+      <CharacterCount />
+    </div>
+  );
+}
+
+function TextInput() {
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+    </div>
+  );
+}
+
+const charCountState = selector({
+  key: "charCountState", // unique ID (with respect to other atoms/selectors)
+  get: ({ get }) => {
+    const text = get(textState);
+
+    return text.length;
+  },
+});
+
+function CharacterCount() {
+  const count = useRecoilValue(charCountState);
+
+  return <>Character Count: {count}</>;
+}
 
 function App() {
   return (
-    <div>
-      <span>recoil</span>
-    </div>
+    <RecoilRoot>
+      <CharacterCounter />
+    </RecoilRoot>
   );
 }
 
