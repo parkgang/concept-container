ddb750c76928f7aa48e727635984654be87e7033
Kyungeun <ruddms936@naver.com>
2021-05-22 08:47:48 +0900

    add: 강제로 2초 기달리도록 설정

--- next.js/js/parkgang.github.io-with-recoil/states/index.js
+++ next.js/js/parkgang.github.io-with-recoil/states/index.js
@@ -1,16 +1,21 @@
-import { atom, selector } from 'recoil';
-import axios from 'axios';
+import { atom, selector } from "recoil";
+import axios from "axios";
 
 export const pageNameState = atom({
-  key: 'pageNameState',
-  default: '',
+  key: "pageNameState",
+  default: "",
 });
 
 export const getNameSelector = selector({
-  key: 'getNameSelector',
+  key: "getNameSelector",
   get: async () => {
     try {
-      const res = await axios.get('http://localhost:3000/api/hello');
+      const res = await axios.get("http://localhost:3000/api/hello");
+      await new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve();
+        }, 2000);
+      });
       return res.data;
     } catch (error) {
       console.log(`getNameSelector 에러 발생`);
