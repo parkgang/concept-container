94713aa53e8152aa2ec752e3ee5c250fe1ce313f
Kyungeun <ruddms936@naver.com>
2021-05-29 11:14:32 +0900

    add: 미들웨어 테스트를 위한 코드 추가

--- nest.js/ts/official-middleware-overview/src/router2/router2.controller.ts
+++ nest.js/ts/official-middleware-overview/src/router2/router2.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
+interface Req {
+  name: string;
+  arg: number;
+}
 
 @Controller('router2')
-export class Router2Controller {}
+export class Router2Controller {
+  @Get()
+  getPing() {
+    return 'Ping: router2';
+  }
+
+  @Post()
+  getTestPost(@Body() value: Req) {
+    console.log(value);
+    return 'router2: Post Sucessue';
+  }
+}
