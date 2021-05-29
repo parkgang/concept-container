e0eeafc0cb868bdc94a3412f518f33d905123636
Kyungeun <ruddms936@naver.com>
2021-05-29 11:21:59 +0900

    docs: 주석 보강

--- nest.js/ts/official-middleware-overview/src/router2/router2.controller.ts
+++ nest.js/ts/official-middleware-overview/src/router2/router2.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 
-interface Req {
+interface TestReq {
   name: string;
   arg: number;
 }
@@ -13,8 +13,9 @@ export class Router2Controller {
   }
 
   @Post()
-  getTestPost(@Body() value: Req) {
-    console.log(value);
+  getTestPost(@Body() payLoad: TestReq) {
+    // type에 맞게 payLoad가 들어오지 않아도 정상적으로 값을 읽을 수 있습니다. 인텔리 센스와 같은 api TestReq 값을 유츄할 때 사용되는 거 같습니다.
+    console.log(`router2 payload: ${JSON.stringify(payLoad)}`);
     return 'router2: Post Sucessue';
   }
 }
