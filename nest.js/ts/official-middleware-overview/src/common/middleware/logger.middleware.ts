099d8f16578c0a281abcb18046edc494b2c2e24f
Kyungeun <ruddms936@naver.com>
2021-05-29 12:02:47 +0900

    up: 인증 여부 검사 로직 추가

--- nest.js/ts/official-middleware-overview/src/common/middleware/logger.middleware.ts
+++ nest.js/ts/official-middleware-overview/src/common/middleware/logger.middleware.ts
@@ -1,11 +1,19 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { HttpStatus, Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     console.log(`Middleware 실행`);
-    console.log(`req: ${JSON.stringify(req.body)}`);
+    console.log(`Middleware req: ${JSON.stringify(req.body)}`);
+
+    if (!req.headers.authorization) {
+      res
+        .status(HttpStatus.UNAUTHORIZED)
+        .json({ errMessage: 'AccessToken이 제공되지 않았습니다.' });
+      return;
+    }
+
     next();
   }
 }
