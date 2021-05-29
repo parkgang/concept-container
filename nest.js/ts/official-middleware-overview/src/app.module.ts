d02aa619d93268a6d684a158c162e44a2b629f21
Kyungeun <ruddms936@naver.com>
2021-05-29 11:26:16 +0900

    add: 기본 middleware template 주입

--- nest.js/ts/official-middleware-overview/src/app.module.ts
+++ nest.js/ts/official-middleware-overview/src/app.module.ts
@@ -1,12 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { Router1Module } from './router1/router1.module';
+
 import { Router2Module } from './router2/router2.module';
+
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [Router1Module, Router2Module],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('cats');
+  }
+}
