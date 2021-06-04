47402904e382478f9a0fbdb52f45b085d386dd82
박경은 <ruddms936@naver.com>
2021-06-04 11:44:23 +0900

    add: 예제 코드 추가

--- react/ts/velog.io-hooks-example/src/components/UseMemo/index.tsx
+++ react/ts/velog.io-hooks-example/src/components/UseMemo/index.tsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+
+import ShowState from "./ShowState";
+
 function UseMemo() {
-  return <></>;
+  const [number, setNumber] = useState(0);
+  const [text, setText] = useState("");
+
+  function increaseNumber() {
+    setNumber((prevState) => prevState + 1);
+  }
+  function decreaseNumber() {
+    setNumber((prevState) => prevState - 1);
+  }
+  function onChangeTextHandler(e: any) {
+    setText(e.target.value);
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <button onClick={increaseNumber}>+</button>
+        <button onClick={decreaseNumber}>-</button>
+        <input type="text" placeholder="Last Name" onChange={onChangeTextHandler} />
+      </div>
+      <ShowState number={number} text={text} />
+    </div>
+  );
 }
 
 export default UseMemo;
