ef94bac06635ddb11fe09d3817cb4b5b98ecfa7e
박경은 <ruddms936@naver.com>
2021-06-04 11:29:47 +0900

    up: 명확한 키워드로 변경

--- react/ts/velog.io-hooks-example/src/components/UseCallback/index.tsx
+++ react/ts/velog.io-hooks-example/src/components/UseCallback/index.tsx
@@ -6,9 +6,9 @@ function UseCallback() {
   const [number, setNumber] = useState(1);
   const [dark, setDark] = useState(false);
 
-  const getItems = () => {
+  function getItems() {
     return [number, number + 1, number + 2];
-  };
+  }
 
   const theme = {
     backgroundColor: dark ? "#333" : "#fff",
