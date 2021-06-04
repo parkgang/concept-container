02eb6ca78069fa53a733a40da0abbd275effc89e
박경은 <ruddms936@naver.com>
2021-06-04 11:35:30 +0900

    style: 코드 인덴트

--- react/ts/velog.io-hooks-example/src/components/UseCallback/List.tsx
+++ react/ts/velog.io-hooks-example/src/components/UseCallback/List.tsx
@@ -6,10 +6,12 @@ type Props = {
 
 function List({ getItems }: Props) {
   const [items, setItems] = useState<number[]>([]);
+
   useEffect(() => {
     setItems(getItems());
     console.log("숫자가 변동되었습니다.");
   }, [getItems]);
+
   return (
     <>
       {items.map((item, index) => (
