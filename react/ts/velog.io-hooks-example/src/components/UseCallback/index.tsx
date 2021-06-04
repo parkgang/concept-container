9c56dd43304de029d8ffd6c698e7cc9af63a097b
박경은 <ruddms936@naver.com>
2021-06-04 11:33:04 +0900

    refactor: 순서 변경

--- react/ts/velog.io-hooks-example/src/components/UseCallback/index.tsx
+++ react/ts/velog.io-hooks-example/src/components/UseCallback/index.tsx
@@ -6,6 +6,11 @@ function UseCallback() {
   const [number, setNumber] = useState(1);
   const [dark, setDark] = useState(false);
 
+  const theme = {
+    backgroundColor: dark ? "#333" : "#fff",
+    color: dark ? "#fff" : "#333",
+  };
+
   // useCallback 미사용
   // function getItems() {
   //   return [number, number + 1, number + 2];
@@ -15,11 +20,6 @@ function UseCallback() {
   const getItems = useCallback(() => {
     return [number, number + 1, number + 2];
   }, [number]);
-
-  const theme = {
-    backgroundColor: dark ? "#333" : "#fff",
-    color: dark ? "#fff" : "#333",
-  };
 
   return (
     <div style={theme}>
