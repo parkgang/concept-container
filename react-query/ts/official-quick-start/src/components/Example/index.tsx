298e9cf7ba8fc0795880547ac37c26a1b471b659
박경은 <ruddms936@naver.com>
2021-06-23 20:36:47 +0900

    fix: ts에 맞게 type 지정 up: fetch가 아닌 axios으로 교체

---
+++ react-query/ts/official-quick-start/src/components/Example/index.tsx
@@ -0,0 +1,39 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+async function Api() {
+  const endPoint = "https://api.github.com/repos/tannerlinsley/react-query";
+  const { data } = await axios.get(endPoint);
+  return data;
+}
+
+function Example() {
+  const { isLoading, error, data } = useQuery<any, Error>("repoData", Api);
+
+  if (isLoading) {
+    return (
+      <>
+        <span>Loading...</span>
+      </>
+    );
+  }
+
+  if (error) {
+    return (
+      <>
+        <span>An error has occurred: </span>
+        <span>{error.message}</span>
+      </>
+    );
+  }
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.description}</p>
+      <strong>👀 {data.subscribers_count}</strong> <strong>✨ {data.stargazers_count}</strong> <strong>🍴 {data.forks_count}</strong>
+    </div>
+  );
+}
+
+export default Example;
