6445c2459338326c813dfc5664cb651efb74ec0c
박경은 <ruddms936@naver.com>
2021-07-20 22:30:11 +0900

    add: 콜백 예제

--- jest/js/daleseo.com-jest-basic/index.test.js
+++ jest/js/daleseo.com-jest-basic/index.test.js
@@ -1,3 +1,22 @@
-test(`hello, world`, () => {
-  expect(1).toBe(1);
+function fetchUser(id, cb) {
+  setTimeout(() => {
+    console.log("wait 0.1 sec.");
+    const user = {
+      id: id,
+      name: "User" + id,
+      email: id + "@test.com",
+    };
+    cb(user);
+  }, 100);
+}
+
+test("fetch a user", (done) => {
+  fetchUser(1, (user) => {
+    expect(user).toEqual({
+      id: 1,
+      name: "User1",
+      email: "1@test.com",
+    });
+    done();
+  });
 });
