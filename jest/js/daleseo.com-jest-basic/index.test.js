6ddc84f7f6e14d520da838e0dee4178476dc5e03
박경은 <ruddms936@naver.com>
2021-07-20 22:44:21 +0900

    add: Promise example

--- jest/js/daleseo.com-jest-basic/index.test.js
+++ jest/js/daleseo.com-jest-basic/index.test.js
@@ -1,22 +1,23 @@
-function fetchUser(id, cb) {
-  setTimeout(() => {
-    console.log("wait 0.1 sec.");
-    const user = {
-      id: id,
-      name: "User" + id,
-      email: id + "@test.com",
-    };
-    cb(user);
-  }, 100);
+function fetchUser(id) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("wait 0.1 sec.");
+      const user = {
+        id: id,
+        name: "User" + id,
+        email: id + "@test.com",
+      };
+      resolve(user);
+    }, 100);
+  });
 }
 
-test("fetch a user", (done) => {
-  fetchUser(1, (user) => {
+test("fetch a user", () => {
+  return fetchUser(1).then((user) => {
     expect(user).toEqual({
       id: 1,
       name: "User1",
       email: "1@test.com",
     });
-    done();
   });
 });
