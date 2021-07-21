992007c214b23a41c172dc08407a1da0ec13b3fe
박경은 <ruddms936@naver.com>
2021-07-22 08:25:06 +0900

    add: grouping

--- jest/js/daleseo.com-jest-basic/src/index.test.js
+++ jest/js/daleseo.com-jest-basic/src/index.test.js
@@ -1,33 +1,19 @@
-const userService = require("./userService");
-const data = require("./data");
+describe("group 1", () => {
+  test("test 1-1", () => {
+    // ...
+  });
 
-afterEach(() => {
-  data.users.splice(0);
+  test("test 1-2", () => {
+    // ...
+  });
 });
 
-beforeEach(() => {
-  data.users.push(
-    { id: 1, email: "user1@test.com" },
-    { id: 2, email: "user2@test.com" },
-    { id: 3, email: "user3@test.com" }
-  );
-});
+describe("group 2", () => {
+  it("test 2-1", () => {
+    // ...
+  });
 
-test("find all users", () => {
-  const users = userService.findAll();
-
-  expect(users).toHaveLength(3);
-  expect(users).toContainEqual({ id: 1, email: "user1@test.com" });
-  expect(users).toContainEqual({ id: 2, email: "user2@test.com" });
-  expect(users).toContainEqual({ id: 3, email: "user3@test.com" });
-});
-
-test("destory a user", () => {
-  const id = 3;
-  const user = data.users.find((user) => user.id === id);
-
-  userService.destroy(id);
-
-  expect(data.users).toHaveLength(2);
-  expect(data.users).not.toContainEqual(user);
+  it("test 2-2", () => {
+    // ...
+  });
 });
