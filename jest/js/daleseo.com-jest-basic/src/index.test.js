bf009ef381dfbc4585657aa61acaed0de95ddc1a
박경은 <ruddms936@naver.com>
2021-07-22 00:09:07 +0900

    add: findAll() 테스트

---
+++ jest/js/daleseo.com-jest-basic/src/index.test.js
@@ -0,0 +1,17 @@
+const userService = require("./userService");
+const data = require("./data");
+
+test("find all users", () => {
+  data.users.push(
+    { id: 1, email: "user1@test.com" },
+    { id: 2, email: "user2@test.com" },
+    { id: 3, email: "user3@test.com" }
+  );
+
+  const users = userService.findAll();
+
+  expect(users).toHaveLength(3);
+  expect(users).toContainEqual({ id: 1, email: "user1@test.com" });
+  expect(users).toContainEqual({ id: 2, email: "user2@test.com" });
+  expect(users).toContainEqual({ id: 3, email: "user3@test.com" });
+});
