6b905e116ae3232a034e894f9752471c415819e6
박경은 <ruddms936@naver.com>
2021-07-23 23:53:09 +0900

    add: 전역 모듈 목 함수 대체

--- jest/js/daleseo.com-jest-basic/src/index.test.js
+++ jest/js/daleseo.com-jest-basic/src/index.test.js
@@ -1,9 +1,15 @@
 const axios = require("axios");
 const userService = require("./userService");
 
-test("findOne fetches data from the API endpoint", async () => {
-  const spyGet = jest.spyOn(axios, "get");
-  await userService.findOne(1);
-  expect(spyGet).toBeCalledTimes(1);
-  expect(spyGet).toBeCalledWith(`https://jsonplaceholder.typicode.com/users/1`);
+test("findOne returns what axios get returns", async () => {
+  axios.get = jest.fn().mockResolvedValue({
+    data: {
+      id: 1,
+      name: "Dale Seo",
+    },
+  });
+
+  const user = await userService.findOne(1);
+  expect(user).toHaveProperty("id", 1);
+  expect(user).toHaveProperty("name", "Dale Seo");
 });
--- jest/js/daleseo.com-jest-basic/src/userService.js
+++ jest/js/daleseo.com-jest-basic/src/userService.js
@@ -3,8 +3,9 @@ const API_ENDPOINT = "https://jsonplaceholder.typicode.com";
 
 module.exports = {
   findOne(id) {
-    return axios
-      .get(`${API_ENDPOINT}/users/${id}`)
-      .then((response) => response.data);
+    return axios.get(`${API_ENDPOINT}/users/${id}`).then((response) => {
+      console.log(response);
+      return response.data;
+    });
   },
 };
