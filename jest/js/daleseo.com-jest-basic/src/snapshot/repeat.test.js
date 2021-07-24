ebae9fc9a5ce8506b36b2c5c55f5be873a46d347
박경은 <ruddms936@naver.com>
2021-07-25 05:33:52 +0900

    add: 스냅샷 결과

--- jest/js/daleseo.com-jest-basic/src/snapshot/repeat.test.js
+++ jest/js/daleseo.com-jest-basic/src/snapshot/repeat.test.js
@@ -1,5 +1,11 @@
 import repeat from "./repeat";
 
 test("repeats words three times", () => {
-  expect(repeat("Test", 3)).toMatchInlineSnapshot();
+  expect(repeat("Test", 3)).toMatchInlineSnapshot(`
+Array [
+  "Test",
+  "Test",
+  "Test",
+]
+`);
 });
