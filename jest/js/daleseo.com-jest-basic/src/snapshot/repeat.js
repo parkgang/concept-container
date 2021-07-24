c6846679e80b35126e73bda9ee4cb4e75df4e4ec
박경은 <ruddms936@naver.com>
2021-07-24 19:57:30 +0900

    edit: 예제 diff를 편리하게 하기 위하여 문법 교체

--- jest/js/daleseo.com-jest-basic/src/snapshot/repeat.js
+++ jest/js/daleseo.com-jest-basic/src/snapshot/repeat.js
@@ -1,7 +1,9 @@
-export default function repeat(word, times = 2) {
+function repeat(word, times = 2) {
   let words = [];
   for (let i = 0; i < times; i++) {
     words.push(word);
   }
   return words;
 }
+
+export default repeat;
