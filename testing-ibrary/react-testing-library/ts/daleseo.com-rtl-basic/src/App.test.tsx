1b7b22260df2b5b48d58b626cccedd38e3e0ff72
박경은 <ruddms936@naver.com>
2021-07-26 18:42:20 +0900

    fix: app test success

--- testing-ibrary/react-testing-library/ts/daleseo.com-rtl-basic/src/App.test.tsx
+++ testing-ibrary/react-testing-library/ts/daleseo.com-rtl-basic/src/App.test.tsx
@@ -2,8 +2,8 @@ import { render, screen } from "@testing-library/react";
 
 import App from "./App";
 
-test("renders learn react link", () => {
+test("hello React Testing Library header", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const h1Element = screen.getByText("React Testing Library");
+  expect(h1Element).toBeInTheDocument();
 });
