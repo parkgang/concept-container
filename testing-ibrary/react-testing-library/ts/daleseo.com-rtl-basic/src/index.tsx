46b8a9ffa642da04680a0aedd34edcbc5a973cac
박경은 <ruddms936@naver.com>
2021-07-27 00:56:18 +0900

    add: async example

--- testing-ibrary/react-testing-library/ts/daleseo.com-rtl-basic/src/index.tsx
+++ testing-ibrary/react-testing-library/ts/daleseo.com-rtl-basic/src/index.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import App from "./App";
 
 import reportWebVitals from "./reportWebVitals";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+      retry: 1,
+    },
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
