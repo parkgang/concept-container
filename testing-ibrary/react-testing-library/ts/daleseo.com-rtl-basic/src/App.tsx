a4acc2cd8014e56fe9510a3dba91aee9118a2cdc
박경은 <ruddms936@naver.com>
2021-07-26 18:42:20 +0900

    fix: app test success

--- testing-ibrary/react-testing-library/ts/daleseo.com-rtl-basic/src/App.tsx
+++ testing-ibrary/react-testing-library/ts/daleseo.com-rtl-basic/src/App.tsx
@@ -4,6 +4,7 @@ import LoginForm from "./components/LoginForm";
 export default function App() {
   return (
     <>
+      <h1>React Testing Library</h1>
       <LoginForm
         onSubmit={() => {
           alert(`로그인 Submit`);
