23fa7c0971f223425916729befd8d8294a44ab57
박경은 <ruddms936@naver.com>
2021-07-27 01:38:11 +0900

    init

---
+++ testing-ibrary/react-testing-library/ts/daleseo.com-rtl-basic/src/components/AsyncCall/AsyncCall.test.tsx
@@ -0,0 +1,41 @@
+import { Suspense } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { render, screen, waitFor } from "@testing-library/react";
+
+import AsyncCall from "./AsyncCall";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+      retry: 1,
+    },
+  },
+});
+
+describe("<AsyncCall />", () => {
+  test("마운트 확인", async () => {
+    async function arrangeRender() {
+      render(
+        <QueryClientProvider client={queryClient}>
+          <Suspense
+            fallback={
+              <div>
+                <h1>로딩 중...</h1>
+              </div>
+            }
+          >
+            <div data-testid="loading">
+              <AsyncCall id={1} />
+            </div>
+          </Suspense>
+        </QueryClientProvider>
+      );
+
+      await waitFor(() => {
+        expect(screen.getByTestId("loading")).toBeInTheDocument();
+      });
+    }
+    await arrangeRender();
+  });
+});
