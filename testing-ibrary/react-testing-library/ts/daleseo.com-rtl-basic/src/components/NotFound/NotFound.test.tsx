d7ad711009f4b44d6ae63de8c76ed2ca2fcd1ea8
박경은 <ruddms936@naver.com>
2021-07-26 15:50:18 +0900

    add: 콘텐츠 검사

--- testing-ibrary/react-testing-library/ts/daleseo.com-rtl-basic/src/components/NotFound/NotFound.test.tsx
+++ testing-ibrary/react-testing-library/ts/daleseo.com-rtl-basic/src/components/NotFound/NotFound.test.tsx
@@ -5,7 +5,9 @@ import NotFound from "./NotFound";
 describe("<NotFound />", () => {
   test("renders header", () => {
     const { getByText } = render(<NotFound path="/abc" />);
-    const header = getByText("Page Not Found");
-    expect(header).toBeInTheDocument();
+    const paragraph = getByText(/^해당 페이지/);
+    expect(paragraph).toHaveTextContent(
+      "해당 페이지(/abc)를 찾을 수 없습니다."
+    );
   });
 });
