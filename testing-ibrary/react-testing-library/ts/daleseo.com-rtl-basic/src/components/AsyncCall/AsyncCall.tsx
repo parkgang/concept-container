abeb754edd594e65fc00f69c2db4c23f0b1ce28b
박경은 <ruddms936@naver.com>
2021-07-27 01:06:56 +0900

    up: dummy data

--- testing-ibrary/react-testing-library/ts/daleseo.com-rtl-basic/src/components/AsyncCall/AsyncCall.tsx
+++ testing-ibrary/react-testing-library/ts/daleseo.com-rtl-basic/src/components/AsyncCall/AsyncCall.tsx
@@ -1,16 +1,37 @@
 import axios from "axios";
 import { useQuery } from "react-query";
 
-export default function AsyncCall() {
-  const { data } = useQuery("todos", async () => {
-    const res = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
+type Dummy = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+type Props = {
+  id: number;
+};
+
+export default function AsyncCall({ id }: Props) {
+  const { data } = useQuery(["Dummy", id], async () => {
+    const res = await axios.get<Dummy>(
+      `https://jsonplaceholder.typicode.com/todos/${id}`
+    );
     return res.data;
   });
+
+  if (data === undefined) {
+    throw new Error(`Dummy 데이터가 undefined 입니다.`);
+  }
 
   return (
     <>
       <h1>AsyncCall</h1>
-      {JSON.stringify(data)}
+      <span>userId: {data.userId}</span> <br />
+      <span>id: {data.id}</span> <br />
+      <span>title: {data.title}</span> <br />
+      <span>completed: {data.completed}</span> <br />
+      <span>json: {JSON.stringify(data)}</span> <br />
     </>
   );
 }
