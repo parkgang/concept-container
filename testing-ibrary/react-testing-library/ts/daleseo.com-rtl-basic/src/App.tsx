984f87a45e912ece09f99f1251bd372f7ca98fcc
박경은 <ruddms936@naver.com>
2021-07-26 14:09:09 +0900

    add: 테스트를 위한 컴포넌트

--- testing-ibrary/react-testing-library/ts/daleseo.com-rtl-basic/src/App.tsx
+++ testing-ibrary/react-testing-library/ts/daleseo.com-rtl-basic/src/App.tsx
@@ -1,7 +1,10 @@
+import NotFound from "./components/NotFound";
+
 export default function App() {
   return (
     <div>
       <h1>App Component</h1>
+      <NotFound path="/test" />
     </div>
   );
 }
