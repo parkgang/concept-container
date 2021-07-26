53bf2b9ca260df71019da94cf99e7cb44b3eb0bf
박경은 <ruddms936@naver.com>
2021-07-26 18:08:14 +0900

    add: Login From

--- testing-ibrary/react-testing-library/ts/daleseo.com-rtl-basic/src/App.tsx
+++ testing-ibrary/react-testing-library/ts/daleseo.com-rtl-basic/src/App.tsx
@@ -1,10 +1,15 @@
 import NotFound from "./components/NotFound";
+import LoginForm from "./components/LoginForm";
 
 export default function App() {
   return (
-    <div>
-      <h1>App Component</h1>
+    <>
+      <LoginForm
+        onSubmit={() => {
+          alert(`로그인 Submit`);
+        }}
+      />
       <NotFound path="/test" />
-    </div>
+    </>
   );
 }
---
+++ testing-ibrary/react-testing-library/ts/daleseo.com-rtl-basic/src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+type Props = {
+  onSubmit(): void;
+};
+
+export default function LoginForm({ onSubmit }: Props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <>
+      <h2>Login</h2>
+      <form onSubmit={onSubmit}>
+        <label>
+          이메일
+          <input
+            type="email"
+            placeholder="user@test.com"
+            value={email}
+            onChange={({ target: { value } }) => setEmail(value)}
+          />
+        </label>
+        <label>
+          비밀번호
+          <input
+            type="password"
+            value={password}
+            onChange={({ target: { value } }) => setPassword(value)}
+          />
+        </label>
+        <button disabled={!email || !password}>로그인</button>
+      </form>
+    </>
+  );
+}
