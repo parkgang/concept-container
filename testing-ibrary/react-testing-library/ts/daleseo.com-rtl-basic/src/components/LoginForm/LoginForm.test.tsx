b139a150a0b0c480aed71acdd7d2e4589112d321
박경은 <ruddms936@naver.com>
2021-07-26 18:38:42 +0900

    add: 로그인 버튼 submit test case

--- testing-ibrary/react-testing-library/ts/daleseo.com-rtl-basic/src/components/LoginForm/LoginForm.test.tsx
+++ testing-ibrary/react-testing-library/ts/daleseo.com-rtl-basic/src/components/LoginForm/LoginForm.test.tsx
@@ -19,4 +19,25 @@ describe("<LoginForm />", () => {
 
     expect(button).toBeEnabled();
   });
+
+  test("submits form when buttion is clicked", () => {
+    const onSubmit = jest.fn().mockImplementation((e) => {
+      e.preventDefault();
+      console.log(`로그인 Submit`);
+    });
+    const { getByText, getByLabelText } = render(
+      <LoginForm onSubmit={onSubmit} />
+    );
+
+    const button = getByText("로그인");
+    const email = getByLabelText("이메일");
+    const password = getByLabelText("비밀번호");
+
+    fireEvent.change(email, { target: { value: "user@test.com" } });
+    fireEvent.change(password, { target: { value: "Test1234" } });
+
+    fireEvent.click(button);
+
+    expect(onSubmit).toHaveBeenCalledTimes(1);
+  });
 });
