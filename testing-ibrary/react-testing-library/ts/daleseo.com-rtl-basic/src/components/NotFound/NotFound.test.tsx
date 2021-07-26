069a2f16ecedca6f078c000274c5a798ec0bb91f
박경은 <ruddms936@naver.com>
2021-07-26 15:51:58 +0900

    add: 렌더링 결과 테스트 코드 추가

--- testing-ibrary/react-testing-library/ts/daleseo.com-rtl-basic/src/components/NotFound/NotFound.test.tsx
+++ testing-ibrary/react-testing-library/ts/daleseo.com-rtl-basic/src/components/NotFound/NotFound.test.tsx
@@ -5,6 +5,12 @@ import NotFound from "./NotFound";
 describe("<NotFound />", () => {
   test("renders header", () => {
     const { getByText } = render(<NotFound path="/abc" />);
+    const header = getByText("Page Not Found");
+    expect(header).toBeInTheDocument();
+  });
+
+  test("renders paragraph", () => {
+    const { getByText } = render(<NotFound path="/abc" />);
     const paragraph = getByText(/^해당 페이지/);
     expect(paragraph).toHaveTextContent(
       "해당 페이지(/abc)를 찾을 수 없습니다."
