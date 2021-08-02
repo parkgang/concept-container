5acaea030860926488d2f898b1cb32aa5f8ace1b
박경은 <ruddms936@naver.com>
2021-08-02 14:52:00 +0900

    add: 명확한 url name 사용

---
+++ react-query/ts/same-query-key/src/pages/NoReactQuery.tsx
@@ -0,0 +1,10 @@
+export default function NoReactQuery() {
+  return (
+    <>
+      <h1>NoReactQuery</h1>
+      <span>해당 페이지에는 React Query가 없습니다.</span>
+      <br />
+      <span>시간이 지나면 캐시데이터가 삭제되는지 확인합니다.</span>
+    </>
+  );
+}
