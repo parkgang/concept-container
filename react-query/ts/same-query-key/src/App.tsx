2e62fe05b50191dbfca74923cd9b2a1f543eb145
박경은 <ruddms936@naver.com>
2021-08-02 14:52:00 +0900

    add: 명확한 url name 사용

--- react-query/ts/same-query-key/src/App.tsx
+++ react-query/ts/same-query-key/src/App.tsx
@@ -1,6 +1,12 @@
 import { ReactQueryDevtools } from "react-query/devtools";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
+import NoReactQuery from "./pages/NoReactQuery";
 import Home1 from "./pages/Home1";
 import Home2 from "./pages/Home2";
 import Home3 from "./pages/Home3";
@@ -14,10 +20,14 @@ export default function App() {
       <Router>
         <Header />
         <Switch>
-          <Route path="/home1" component={Home1} />
-          <Route path="/home2" component={Home2} />
-          <Route path="/home3" component={Home3} />
-          <Route path="/home4" component={Home4} />
+          <Route exact path="/">
+            <Redirect to="/no-react-query" />
+          </Route>
+          <Route path="/no-react-query" component={NoReactQuery} />
+          <Route path="/query1" component={Home1} />
+          <Route path="/query2" component={Home2} />
+          <Route path="/query3" component={Home3} />
+          <Route path="/query4" component={Home4} />
         </Switch>
       </Router>
       <ReactQueryDevtools initialIsOpen={false} />
--- react-query/ts/same-query-key/src/components/Header.tsx
+++ react-query/ts/same-query-key/src/components/Header.tsx
@@ -3,25 +3,31 @@ import { useHistory } from "react-router-dom";
 export default function Header() {
   const history = useHistory();
 
-  function handleHome1() {
-    history.push(`home1`);
+  function handleNoReactQuery() {
+    history.push(`no-react-query`);
   }
-  function handleHome2() {
-    history.push(`home2`);
+  function handleQuery1() {
+    history.push(`query1`);
   }
-  function handleHome3() {
-    history.push(`home3`);
+  function handleQuery2() {
+    history.push(`query2`);
   }
-  function handleHome4() {
-    history.push(`home4`);
+  function handleQuery3() {
+    history.push(`query3`);
+  }
+  function handleQuery4() {
+    history.push(`query4`);
   }
 
   return (
     <>
-      <button onClick={handleHome1}>Home1</button>
-      <button onClick={handleHome2}>Home2</button>
-      <button onClick={handleHome3}>Home3</button>
-      <button onClick={handleHome4}>Home4</button>
+      <div style={{ padding: "1rem", border: "1px solid" }}>
+        <button onClick={handleNoReactQuery}>no-react-query</button>
+        <button onClick={handleQuery1}>query1</button>
+        <button onClick={handleQuery2}>query2</button>
+        <button onClick={handleQuery3}>query3</button>
+        <button onClick={handleQuery4}>query4</button>
+      </div>
     </>
   );
 }
