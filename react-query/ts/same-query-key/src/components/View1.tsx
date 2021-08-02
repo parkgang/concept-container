fcc4b56f04fb71eba0ac96d33ccea8f771a26c6b
박경은 <ruddms936@naver.com>
2021-08-02 15:10:57 +0900

    add: query state를 알기 쉽도록 log 강화

--- react-query/ts/same-query-key/src/components/View1.tsx
+++ react-query/ts/same-query-key/src/components/View1.tsx
@@ -1,17 +1,42 @@
+import { useState, useEffect } from "react";
 import { useQuery } from "react-query";
+
+import View2 from "../components/View2";
 
 import { View1Api } from "../libs/api";
 
 export default function View1() {
+  const [flag, setFlag] = useState<boolean>(false);
+
   const { data } = useQuery("View1Api", View1Api, {
-    staleTime: 1000 * 30,
+    staleTime: 1000 * 1,
     cacheTime: 1000 * 5,
+    onSuccess() {
+      console.log(`View1 onSuccess`);
+    },
+    onSettled() {
+      console.log(`View1 onSettled`);
+    },
   });
+
+  useEffect(() => {
+    console.log(`View1 처음 마운트`);
+  }, []);
 
   return (
     <>
       <h1>View1</h1>
       {JSON.stringify(data)}
+      <br />
+      <button
+        onClick={() => {
+          setFlag((x) => !x);
+        }}
+      >
+        View2 렌더링 시작
+      </button>
+      <hr />
+      {flag && <View2 />}
     </>
   );
 }
--- react-query/ts/same-query-key/src/components/View2.tsx
+++ react-query/ts/same-query-key/src/components/View2.tsx
@@ -1,16 +1,41 @@
+import { useState, useEffect } from "react";
 import { useQuery } from "react-query";
+
+import View3 from "../components/View3";
 
 import { View2Api } from "../libs/api";
 
 export default function View2() {
+  const [flag, setFlag] = useState<boolean>(false);
+
   const { data } = useQuery("View1Api", View2Api, {
-    cacheTime: 1000 * 10,
+    cacheTime: 1000 * 6,
+    onSuccess() {
+      console.log(`View2 onSuccess`);
+    },
+    onSettled() {
+      console.log(`View2 onSettled`);
+    },
   });
+
+  useEffect(() => {
+    console.log("View2 처음 마운트");
+  }, []);
 
   return (
     <>
       <h1>View2</h1>
       {JSON.stringify(data)}
+      <br />
+      <button
+        onClick={() => {
+          setFlag((x) => !x);
+        }}
+      >
+        View3 렌더링 시작
+      </button>
+      <hr />
+      {flag && <View3 />}
     </>
   );
 }
