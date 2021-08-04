ce8c10342215f6c2654398462c56270efc5731b3
박경은 <ruddms936@naver.com>
2021-08-05 06:49:58 +0900

    add: 스프레드

--- es6/js/study/sandbox.js
+++ es6/js/study/sandbox.js
@@ -1,1 +1,12 @@
-console.log(`sandbox`);
+// 연습장입니다.
+
+const item = { type: "👔", size: "M" };
+const detail = { price: 20, made: "Korea", gender: "M" };
+
+// ✅ Good Code ✨
+const shirt0 = Object.assign(item, detail);
+console.log(shirt0);
+
+// ✅ Better! Code ✨
+const shirt = { ...item, ...detail, price: 30 };
+console.log(shirt);
