386171d259f6cf4cf121ca5e27e1be2ff5721e94
박경은 <ruddms936@naver.com>
2021-08-16 19:45:58 +0900

    init

---
+++ testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/README.md
@@ -0,0 +1,3 @@
+# Hi
+
+1. [해당 글](https://velog.io/@velopert/tdd-with-react-testing-library) 으로 진행된 실습입니다.
---
+++ testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="App">
+      <h1>App</h1>
+    </div>
+  );
+}
