8d55ec0fff662b0edc4dde0490fef4f15e3a4ba4
박경은 <ruddms936@naver.com>
2021-08-21 16:09:17 +0900

    add: 할일 완료시 스타일 적용

--- testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/TodoItem.test.tsx
+++ testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/TodoItem.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import TodoItem from "./TodoItem";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 describe("<TodoItem />", () => {
   const sampleTodo = {
@@ -27,5 +27,29 @@ describe("<TodoItem />", () => {
     const { span, button } = setup();
     expect(span).toBeTruthy();
     expect(button).toBeTruthy();
+  });
+
+  it("shows line-through on span when done is true", () => {
+    const { span } = setup({ todo: { ...sampleTodo, done: true } });
+    expect(span).toHaveStyle("text-decoration: line-through;");
+  });
+
+  it("does not show line-through on span when done is false", () => {
+    const { span } = setup({ todo: { ...sampleTodo, done: false } });
+    expect(span).not.toHaveStyle("text-decoration: line-through;");
+  });
+
+  it("calls onToggle", () => {
+    const onToggle = jest.fn();
+    const { span } = setup({ onToggle });
+    fireEvent.click(span);
+    expect(onToggle).toBeCalledWith(sampleTodo.id);
+  });
+
+  it("calls onRemove", () => {
+    const onRemove = jest.fn();
+    const { button } = setup({ onRemove });
+    fireEvent.click(button);
+    expect(onRemove).toBeCalledWith(sampleTodo.id);
   });
 });
--- testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/TodoItem.tsx
+++ testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/TodoItem.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useCallback } from "react";
 
-const TodoItem = ({ todo }) => {
+const TodoItem = ({ todo, onToggle, onRemove }) => {
   const { id, text, done } = todo;
+  const toggle = useCallback(() => onToggle(id), [id, onToggle]);
+  const remove = useCallback(() => onRemove(id), [id, onRemove]);
 
   return (
     <li>
-      <span>{text}</span>
-      <button>삭제</button>
+      <span
+        style={{
+          textDecoration: done ? "line-through" : "none",
+        }}
+        onClick={toggle}
+      >
+        {text}
+      </span>
+      <button onClick={remove}>삭제</button>
     </li>
   );
 };
