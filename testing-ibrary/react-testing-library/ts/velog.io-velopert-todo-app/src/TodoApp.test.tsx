af36f10b939dd6f9a3d823adbf22a1a166210df7
박경은 <ruddms936@naver.com>
2021-08-21 16:24:08 +0900

    add: 더미 데이터

--- testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/TodoApp.test.tsx
+++ testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/TodoApp.test.tsx
@@ -8,4 +8,10 @@ describe("<TodoApp />", () => {
     getByText("등록"); // TodoForm 존재유무 확인
     getByTestId("TodoList"); // TodoList 존재유무 확인
   });
+
+  it("renders two defaults todos", () => {
+    const { getByText } = render(<TodoApp />);
+    getByText("TDD 배우기");
+    getByText("react-testing-library 사용하기");
+  });
 });
--- testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/TodoApp.tsx
+++ testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/TodoApp.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
 
 const TodoApp = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "TDD 배우기",
+      done: true,
+    },
+    {
+      id: 2,
+      text: "react-testing-library 사용하기",
+      done: true,
+    },
+  ]);
   return (
     <>
-      <TodoForm data-testid="helloworld" />
-      <TodoList todos={[]} />
+      <TodoForm />
+      <TodoList todos={todos} />
     </>
   );
 };
