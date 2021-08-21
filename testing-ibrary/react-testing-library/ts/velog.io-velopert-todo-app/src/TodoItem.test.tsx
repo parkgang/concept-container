717674f0730e933ef0f26d2f8a6359b6b80db9cc
박경은 <ruddms936@naver.com>
2021-08-21 16:01:11 +0900

    add: TodoItem Component

---
+++ testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/TodoItem.test.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+import { render } from "@testing-library/react";
+
+describe("<TodoItem />", () => {
+  const sampleTodo = {
+    id: 1,
+    text: "TDD 배우기",
+    done: false,
+  };
+
+  const setup = (props = {}) => {
+    const initialProps = { todo: sampleTodo };
+    const utils = render(<TodoItem {...initialProps} {...props} />);
+    const { getByText } = utils;
+    const todo = props.todo || initialProps.todo;
+    const span = getByText(todo.text);
+    const button = getByText("삭제");
+    return {
+      ...utils,
+      span,
+      button,
+    };
+  };
+
+  it("has span and button", () => {
+    const { span, button } = setup();
+    expect(span).toBeTruthy();
+    expect(button).toBeTruthy();
+  });
+});
