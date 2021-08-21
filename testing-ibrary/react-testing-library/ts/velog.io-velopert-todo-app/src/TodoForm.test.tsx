fa94a98395e669def3c4cf356fac941ea16dc5c8
박경은 <ruddms936@naver.com>
2021-08-21 15:52:24 +0900

    add: inpit status management

--- testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/TodoForm.test.tsx
+++ testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/TodoForm.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import TodoForm from "./TodoForm";
 
 describe("<TodoForm />", () => {
@@ -7,5 +7,16 @@ describe("<TodoForm />", () => {
     const { getByText, getByPlaceholderText } = render(<TodoForm />);
     getByPlaceholderText("할 일을 입력하세요"); // input 이 있는지 확인
     getByText("등록"); // button이 있는지 확인
+  });
+
+  it("changes input", () => {
+    const { getByPlaceholderText } = render(<TodoForm />);
+    const input = getByPlaceholderText("할 일을 입력하세요");
+    fireEvent.change(input, {
+      target: {
+        value: "TDD 배우기",
+      },
+    });
+    expect(input).toHaveAttribute("value", "TDD 배우기");
   });
 });
--- testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/TodoForm.tsx
+++ testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/TodoForm.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 
 const TodoForm = () => {
+  const [value, setValue] = useState("");
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
   return (
     <form>
-      <input placeholder="할 일을 입력하세요" />
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">등록</button>
     </form>
   );
