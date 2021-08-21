c811938db75922e53130724927f2399bc1116a87
박경은 <ruddms936@naver.com>
2021-08-21 16:14:53 +0900

    add: event handler test

--- testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/TodoList.test.tsx
+++ testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/TodoList.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import TodoList from "./TodoList";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 describe("<TodoList />", () => {
   const sampleTodos = [
@@ -20,5 +20,19 @@ describe("<TodoList />", () => {
     const { getByText } = render(<TodoList todos={sampleTodos} />);
     getByText(sampleTodos[0].text);
     getByText(sampleTodos[1].text);
+  });
+
+  it("calls onToggle and onRemove", () => {
+    const onToggle = jest.fn();
+    const onRemove = jest.fn();
+    const { getByText, getAllByText } = render(
+      <TodoList todos={sampleTodos} onToggle={onToggle} onRemove={onRemove} />
+    );
+
+    fireEvent.click(getByText(sampleTodos[0].text));
+    expect(onToggle).toBeCalledWith(sampleTodos[0].id);
+
+    fireEvent.click(getAllByText("삭제")[0]); // 첫번째 삭제 버튼을 클릭
+    expect(onRemove).toBeCalledWith(sampleTodos[0].id);
   });
 });
--- testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/TodoList.tsx
+++ testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/TodoList.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onToggle, onRemove }) => {
   return (
     <ul>
       {todos.map((todo) => (
-        <TodoItem todo={todo} key={todo.id} />
+        <TodoItem
+          todo={todo}
+          key={todo.id}
+          onToggle={onToggle}
+          onRemove={onRemove}
+        />
       ))}
     </ul>
   );
