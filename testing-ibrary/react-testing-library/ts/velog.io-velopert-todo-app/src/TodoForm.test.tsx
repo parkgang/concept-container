fa48a3e0d27c4f8ad8ae36ee9734729e036134eb
박경은 <ruddms936@naver.com>
2021-08-21 15:47:59 +0900

    init: TodoForm

---
+++ testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/TodoForm.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import TodoForm from "./TodoForm";
+
+describe("<TodoForm />", () => {
+  it("has input and a button", () => {
+    const { getByText, getByPlaceholderText } = render(<TodoForm />);
+    getByPlaceholderText("할 일을 입력하세요"); // input 이 있는지 확인
+    getByText("등록"); // button이 있는지 확인
+  });
+});
