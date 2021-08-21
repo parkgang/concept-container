198a252df9c04278c55b22b95e0e68087eb1a53d
박경은 <ruddms936@naver.com>
2021-08-21 15:55:33 +0900

    add: submit event management

--- testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/TodoForm.tsx
+++ testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/TodoForm.tsx
@@ -1,13 +1,21 @@
 import React, { useState, useCallback } from "react";
 
-const TodoForm = () => {
+const TodoForm = ({ onInsert }) => {
   const [value, setValue] = useState("");
   const onChange = useCallback((e) => {
     setValue(e.target.value);
   }, []);
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue("");
+      e.preventDefault(); // 새로고침을 방지함
+    },
+    [onInsert, value]
+  );
 
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <input
         placeholder="할 일을 입력하세요"
         value={value}
