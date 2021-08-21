2238d0434a60ce211ea9b1b0d77b3a537911c7bb
박경은 <ruddms936@naver.com>
2021-08-21 16:38:44 +0900

    add: 새항목 추가 기능 테스트

--- testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/TodoApp.test.tsx
+++ testing-ibrary/react-testing-library/ts/velog.io-velopert-todo-app/src/TodoApp.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import TodoApp from "./TodoApp";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 describe("<TodoApp />", () => {
   it("renders TodoForm and TodoList", () => {
@@ -12,6 +12,19 @@ describe("<TodoApp />", () => {
   it("renders two defaults todos", () => {
     const { getByText } = render(<TodoApp />);
     getByText("TDD 배우기");
-    getByText("react-testing-library 사용하기");
+    getByText("react-testing-library 배우기");
+  });
+
+  it("creates new todo", () => {
+    const { getByPlaceholderText, getByText } = render(<TodoApp />);
+    // 이벤트를 발생시켜서 새 항목을 추가하면
+    fireEvent.change(getByPlaceholderText("할 일을 입력하세요"), {
+      target: {
+        value: "새 항목 추가하기",
+      },
+    });
+    fireEvent.click(getByText("등록"));
+    // 해당 항목이 보여져야합니다.
+    getByText("새 항목 추가하기");
   });
 });
