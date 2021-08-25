18f349afd5152c248e499bb818e6f974b2a67056
박경은 <ruddms936@naver.com>
2021-08-25 13:03:42 +0900

    up: 비구조화 할당 적용

--- react/ts/velog.io-dfd1123-portal-example/src/ModalPortals.tsx
+++ react/ts/velog.io-dfd1123-portal-example/src/ModalPortals.tsx
@@ -1,12 +1,13 @@
 import { ReactNode } from "react";
-import ReactDOM from "react-dom";
+import { createPortal } from "react-dom";
 
 type Props = {
   children: ReactNode;
 };
 
 export default function ModalPortals({ children }: Props) {
-  const modalElement = document.querySelector("#modal");
-  return ReactDOM.createPortal(children, modalElement as Element);
+  const modalElement = document.getElementById("modal");
+
   // createPortal(ModalPortals안에서 랜더링될 컴포넌트, 랜더링 시킬 상위 DOM Element)
+  return createPortal(children, modalElement as Element);
 }
