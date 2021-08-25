228a5ee68ec69d0b250df86c6bd2cab2c703b344
박경은 <ruddms936@naver.com>
2021-08-25 12:33:35 +0900

    add: hello, world portal

---
+++ react/ts/velog.io-dfd1123-portal-example/src/Modal.tsx
@@ -0,0 +1,30 @@
+type Props = {
+  show: any;
+  handleModalShow: any;
+};
+
+export default function Modal({ show, handleModalShow }: Props) {
+  return (
+    <div className={"modal-wrap " + (show ? "active" : "")}>
+      <div
+        className="overlay"
+        onClick={() => {
+          handleModalShow(false);
+        }}
+      ></div>
+      <div className="modal-con">
+        <div className="contents">모달이 열렸다!</div>
+        <div className="bottom">
+          <button
+            type="button"
+            onClick={() => {
+              handleModalShow(false);
+            }}
+          >
+            모달 닫기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
