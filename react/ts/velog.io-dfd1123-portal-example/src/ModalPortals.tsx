71624366eeceb7b428b4e9690bee1e34c8e20241
박경은 <ruddms936@naver.com>
2021-08-25 12:37:01 +0900

    fix: type 수정

--- react/ts/velog.io-dfd1123-portal-example/src/ModalPortals.tsx
+++ react/ts/velog.io-dfd1123-portal-example/src/ModalPortals.tsx
@@ -1,7 +1,12 @@
+import { ReactNode } from "react";
 import ReactDOM from "react-dom";
 
-export default function ModalPortals({ children }) {
+type Props = {
+  children: ReactNode;
+};
+
+export default function ModalPortals({ children }: Props) {
   const modalElement = document.querySelector("#modal");
-  return ReactDOM.createPortal(children, modalElement);
+  return ReactDOM.createPortal(children, modalElement as Element);
   // createPortal(ModalPortals안에서 랜더링될 컴포넌트, 랜더링 시킬 상위 DOM Element)
 }
