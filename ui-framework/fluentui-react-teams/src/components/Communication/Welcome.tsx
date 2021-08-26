3ecb5c717b07d26dd77366f229620e87ab193738
박경은 <ruddms936@naver.com>
2021-08-26 12:16:08 +0900

    add: event handler 등록

--- ui-framework/fluentui-react-teams/src/components/Communication/Welcome.tsx
+++ ui-framework/fluentui-react-teams/src/components/Communication/Welcome.tsx
@@ -5,8 +5,22 @@ import {
 } from "@fluentui/react-teams";
 
 export default function Welcome() {
+  function handleInteraction(target: string) {
+    console.log(target);
+  }
+
   const props: TCommunicationProps = {
     option: CommunicationOptions.Welcome,
+    fields: {
+      title: "title",
+      desc: "desc",
+      actions: {
+        primary: { label: "사용 동의", target: "consent" },
+        secondary: undefined,
+        tertiary: { label: "도움말", target: "help" },
+      },
+    },
+    onInteraction: ({ target }) => handleInteraction(target),
   };
 
   return (
