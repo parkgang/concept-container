9aa9f6982feec45a4dc3f21877062a0d898f22fc
박경은 <ruddms936@naver.com>
2021-08-26 12:53:48 +0900

    add: 2가지 샘플 추가

--- ui-framework/fluentui-react-teams/src/App.tsx
+++ ui-framework/fluentui-react-teams/src/App.tsx
@@ -1,11 +1,15 @@
 import { Provider as RTProvider, themeNames } from "@fluentui/react-teams";
 
 import Welcome from "components/Communication/Welcome";
+import Error from "components/Communication/Error";
 
 export default function App() {
   return (
     <RTProvider themeName={themeNames.Default} lang="en-US">
       <Welcome />
+      <hr />
+      <Error />
+      <hr />
     </RTProvider>
   );
 }
--- ui-framework/fluentui-react-teams/src/components/Communication/Error.tsx
+++ ui-framework/fluentui-react-teams/src/components/Communication/Error.tsx
@@ -4,20 +4,18 @@ import {
   CommunicationOptions,
 } from "@fluentui/react-teams";
 
-export default function Welcome() {
+export default function Error() {
   function handleInteraction(target: string) {
     console.log(target);
   }
 
   const props: TCommunicationProps = {
-    option: CommunicationOptions.Welcome,
+    option: CommunicationOptions.Error,
     fields: {
-      title: "처음이신가요?",
-      desc: "앱 사용이 처음인 경우, 최초 1회 사용 동의가 필요합니다.",
+      title: "앗, 이런!",
+      desc: "에러메시지가 들어갑니다.",
       actions: {
-        primary: { label: "사용 동의", target: "consent" },
-        secondary: undefined,
-        tertiary: { label: "도움말", target: "help" },
+        primary: { label: "새로고침", target: "refresh" },
       },
     },
     onInteraction: ({ target }) => handleInteraction(target),
