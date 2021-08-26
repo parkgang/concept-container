bf25e0ba49ab531231a3f4f78830245a71186272
박경은 <ruddms936@naver.com>
2021-08-26 13:19:25 +0900

    add: theme handle 추가

--- ui-framework/fluentui-react-teams/src/App.tsx
+++ ui-framework/fluentui-react-teams/src/App.tsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import { Provider as RTProvider, themeNames } from "@fluentui/react-teams";
 
 import Welcome from "components/Communication/Welcome";
 import Error from "components/Communication/Error";
 
 export default function App() {
+  const [rtTheme, setRtTheme] = useState<themeNames>(themeNames.Default);
+
+  function handleRtTheme(theme: themeNames) {
+    return () => setRtTheme(theme);
+  }
+
   return (
-    <RTProvider themeName={themeNames.Default} lang="en-US">
-      <Welcome />
-      <hr />
-      <Error />
-      <hr />
-    </RTProvider>
+    <>
+      <header>
+        <button onClick={handleRtTheme(themeNames.Default)}>Default</button>
+        <button onClick={handleRtTheme(themeNames.Dark)}>Dark</button>
+        <button onClick={handleRtTheme(themeNames.HighContrast)}>
+          HighContrast
+        </button>
+      </header>
+      <RTProvider themeName={rtTheme} lang="en-US">
+        <Welcome />
+        <hr />
+        <Error />
+        <hr />
+      </RTProvider>
+    </>
   );
 }
