0d1cd686bb0ad80d9f1813e4bb4654c2d85ac2ba
박경은 <ruddms936@naver.com>
2021-08-26 12:05:14 +0900

    add: base sapmlate 추가

--- ui-framework/fluentui-react-teams/src/App.tsx
+++ ui-framework/fluentui-react-teams/src/App.tsx
@@ -1,7 +1,19 @@
+import {
+  Provider as RTProvider,
+  themeNames,
+  Communication,
+  TCommunicationProps,
+  CommunicationOptions,
+} from "@fluentui/react-teams";
+
 export default function App() {
+  const props: TCommunicationProps = {
+    option: CommunicationOptions.Welcome,
+  };
+
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <RTProvider themeName={themeNames.Default} lang="en-US">
+      <Communication {...props} />
+    </RTProvider>
   );
 }
