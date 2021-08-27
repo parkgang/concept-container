f4849f2ab53476193e9635c77deca4d8de585f4c
박경은 <ruddms936@naver.com>
2021-08-28 02:22:52 +0900

    add: 리렌더링 예제 코드

--- react/ts/rerendering-playground/src/App.tsx
+++ react/ts/rerendering-playground/src/App.tsx
@@ -1,7 +1,51 @@
-export default function App() {
+import { useState } from "react";
+
+function Test1({ value }: any) {
   return (
     <>
-      <h1>App</h1>
+      <article>
+        <h1>Test1</h1>
+        {JSON.stringify(value)}
+        <Test2 />
+      </article>
+    </>
+  );
+}
+
+function Test2() {
+  return (
+    <>
+      <article>
+        <h1>Test2</h1>
+      </article>
+    </>
+  );
+}
+
+export default function App() {
+  const [, setFlag1] = useState<boolean>(false);
+  const [flag2, setFlag2] = useState<boolean>(false);
+
+  return (
+    <>
+      <main>
+        <h1>App</h1>
+        <button
+          onClick={() => {
+            setFlag1((v) => !v);
+          }}
+        >
+          setFlag1
+        </button>
+        <button
+          onClick={() => {
+            setFlag2((v) => !v);
+          }}
+        >
+          setFlag2
+        </button>
+        <Test1 value={flag2} />
+      </main>
     </>
   );
 }
