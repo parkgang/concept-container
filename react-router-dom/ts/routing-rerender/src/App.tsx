4f5ec8d5a5bdf60047292eff86563ca756cdfba9
박경은 <ruddms936@naver.com>
2021-09-02 14:38:03 +0900

    add: NotFound

--- react-router-dom/ts/routing-rerender/src/App.tsx
+++ react-router-dom/ts/routing-rerender/src/App.tsx
@@ -4,6 +4,7 @@ import Nav from "components/Nav";
 import APage from "pages/APage";
 import BPage from "pages/BPage";
 import CPage from "pages/CPage";
+import NotFoundPage from "pages/NotFoundPage";
 
 export const APagePath = "/a";
 export const BPagePath = "/b";
@@ -18,6 +19,7 @@ export default function App() {
           <Route path={APagePath} component={APage} />
           <Route path={BPagePath} component={BPage} />
           <Route path={CPagePath} component={CPage} />
+          <Route component={NotFoundPage} />
         </Switch>
       </Router>
     </>
