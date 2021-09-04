d8afbccb9b7f6dcde0d4af903824e9627a6708ef
박경은 <ruddms936@naver.com>
2021-09-04 22:18:22 +0900

    style: 변경된 formatter 설정 적용

--- next.js/js/official-tutorial-blog/components/layout.js
+++ next.js/js/official-tutorial-blog/components/layout.js
@@ -1,18 +1,21 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from './layout.module.css';
-import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
-const name = 'Your Name';
-export const siteTitle = 'Next.js Sample Website';
+const name = "Your Name";
+export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="Learn how to build a personal website using Next.js" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
