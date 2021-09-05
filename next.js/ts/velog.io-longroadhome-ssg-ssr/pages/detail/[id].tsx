d1b0ad10eb0ec442ebf276c70834c7aac587c282
박경은 <ruddms936@naver.com>
2021-09-05 21:51:26 +0900

    add: CSR 예제 코드 추가

--- next.js/ts/velog.io-longroadhome-ssg-ssr/pages/detail/[id].tsx
+++ next.js/ts/velog.io-longroadhome-ssg-ssr/pages/detail/[id].tsx
@@ -39,3 +39,44 @@ export default function Detail({
     </div>
   );
 }
+
+/**
+ * 위와 동일한 코드인데 CSR으로 처리하는 코드
+ */
+/*
+export default function Detail() {
+  const [item, setItem] = useState<Post>();
+
+  const router = useRouter();
+  const id = router.query.id;
+
+  useEffect(() => {
+    (async () => {
+      // router.query의 경우 마운트되지 않으면 조회되지 않는 것으로 확인됩니다.
+      if (id) {
+        console.log(id);
+        const { data } = await axios.get(
+          `https://jsonplaceholder.typicode.com/posts/${id}`
+        );
+        setItem(data);
+      }
+    })();
+  }, [id]);
+
+  if (item) {
+    return (
+      <div className="Detail">
+        <h1>{item.title}</h1>
+        <p>{item.body}</p>
+        <p>{item.id}번째 게시글</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h3>Loading...</h3>
+    </div>
+  );
+}
+*/
