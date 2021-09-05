1c541a48e5982e01f57dba3a26c89a1c4ab68bdf
박경은 <ruddms936@naver.com>
2021-09-05 20:00:55 +0900

    add: no SSG 샘플 코드

--- next.js/ts/velog.io-longroadhome-ssg-ssr/pages/_app.tsx
+++ next.js/ts/velog.io-longroadhome-ssg-ssr/pages/_app.tsx
@@ -1,7 +1,9 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />;
+    </>
+  );
 }
-export default MyApp;
