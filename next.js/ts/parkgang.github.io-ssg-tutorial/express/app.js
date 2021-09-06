1252fca2c32ab1a84d0d983ba84f39c8a6e33670
박경은 <ruddms936@naver.com>
2021-09-07 03:56:46 +0900

    add: 샘플 코드 마이그레이션

--- next.js/ts/parkgang.github.io-ssg-tutorial/express/app.js
+++ next.js/ts/parkgang.github.io-ssg-tutorial/express/app.js
@@ -1,1 +1,13 @@
-console.log(`expressjs`);
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
