b9ea9707a0d4fa09a08d2d168fd45e9b0020c4c3
박경은 <ruddms936@naver.com>
2021-09-07 21:46:36 +0900

    move: 블로그 예제 코드는 다른 repo로 이동

--- next.js/ts/parkgang.github.io-ssg-tutorial/README.md
+++
@@ -1,5 +0,0 @@
-# Hi
-
-1. next.js의 SSG의 여러 Case를 테스트하기 위한 프로젝트입니다.
-1. [해당 글](https://parkgang.github.io/next.js/lets-properly-understand-and-use-the-ssg-of-next.js/)을 확인하실 수 있습니다.
-1. next.js serverless 상태로 static 배포 방법을 소개합니다.
--- next.js/ts/parkgang.github.io-ssg-tutorial/next/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  trailingSlash: true,
-  images: {
-    // [next export시 Image Optimization 에러가 발생하여 타사 로더 사용](https://nextjs.org/docs/messages/export-image-api)
-    loader: "imgix",
-    path: "",
-  },
-};
