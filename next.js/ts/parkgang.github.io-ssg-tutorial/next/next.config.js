c47d66b4d990b4455cd7ff45acd7e0fe5b1bd799
박경은 <ruddms936@naver.com>
2021-09-07 14:57:10 +0900

    add: image 타사 로더 사용

--- next.js/ts/parkgang.github.io-ssg-tutorial/next/next.config.js
+++ next.js/ts/parkgang.github.io-ssg-tutorial/next/next.config.js
@@ -2,6 +2,8 @@
 module.exports = {
   trailingSlash: true,
   images: {
-    domains: ["images.dog.ceo"],
+    // [next export시 Image Optimization 에러가 발생하여 타사 로더 사용](https://nextjs.org/docs/messages/export-image-api)
+    loader: "imgix",
+    path: "",
   },
 };
