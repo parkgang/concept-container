63e23cbb3dca994125cafbcc1adbea514801e09c
박경은 <ruddms936@naver.com>
2021-10-05 11:21:30 +0900

    add: teams manifest icon sample

---
+++ ui-framework/fluentui-react-northstar/src/components/TeamsManifestBrandingIcon/ColorIcon.tsx
@@ -0,0 +1,30 @@
+import { Provider, teamsTheme, AppsIcon } from "@fluentui/react-northstar";
+import styled from "styled-components";
+
+const Reset = styled.div`
+  width: 192px;
+  height: 192px;
+  /* 중앙 정렬 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & > span > svg {
+    width: 96px;
+    height: 96px;
+  }
+`;
+
+/**
+ * 상점 및 대부분의 시나리오에서 표시됩니다. 아이콘은 중앙에 96x96픽셀 기호가 포함된 총 192x192픽셀이어야 합니다.
+ */
+export default function ColorIcon() {
+  return (
+    <>
+      <Provider theme={teamsTheme}>
+        <Reset>
+          <AppsIcon />
+        </Reset>
+      </Provider>
+    </>
+  );
+}
---
+++ ui-framework/fluentui-react-northstar/src/components/TeamsManifestBrandingIcon/OutlineIcon.tsx
@@ -0,0 +1,42 @@
+import {
+  Provider,
+  teamsTheme,
+  teamsDarkTheme,
+  AppsIcon,
+} from "@fluentui/react-northstar";
+import styled from "styled-components";
+
+const Reset = styled.div`
+  width: 32px;
+  height: 32px;
+  color: white;
+  & > span > svg {
+    width: 32px;
+    height: 32px;
+  }
+`;
+
+/**
+ * 앱이 사용 중일 때 주로 Teams의 왼쪽에 표시됩니다. 아이콘은 32x32픽셀이어야 하며 흰색 또는 투명해야 합니다.
+ * 여기서 글씨가 검은색이면 다크 테마시 표시가 안됩니다. 흰색으로 꼭 하도록 합니다.
+ * 테마에 따라 테스트를 위해서 backgroundColor를 teamsDarkTheme로 바꿔놓은 것입니다.
+ */
+export default function OutlineIcon() {
+  return (
+    <>
+      <Provider theme={teamsTheme}>
+        <div
+          style={{
+            width: "5rem",
+            height: "5rem",
+            backgroundColor: teamsDarkTheme.siteVariables.bodyBackground,
+          }}
+        >
+          <Reset>
+            <AppsIcon />
+          </Reset>
+        </div>
+      </Provider>
+    </>
+  );
+}
