118b8f619f487b77a74ef6510cd510d0929514d1
박경은 <ruddms936@naver.com>
2021-10-05 12:17:46 +0900

    fix: 뒷 배경을 쉽게 날릴 수 있도록 초록색으로 지정

--- ui-framework/fluentui-react-northstar/src/components/TeamsManifestBrandingIcon/ColorIcon.tsx
+++ ui-framework/fluentui-react-northstar/src/components/TeamsManifestBrandingIcon/ColorIcon.tsx
@@ -1,15 +1,10 @@
-import {
-  Provider,
-  teamsTheme,
-  teamsDarkTheme,
-  AppsIcon,
-} from "@fluentui/react-northstar";
+import { Provider, teamsTheme, AppsIcon } from "@fluentui/react-northstar";
 import styled from "styled-components";
 
 const Reset = styled.div`
   width: 192px;
   height: 192px;
-  background-color: ${teamsDarkTheme.siteVariables.bodyBackground};
+  background-color: green;
   /* 중앙 정렬 */
   display: flex;
   justify-content: center;
@@ -34,7 +29,7 @@ const Reset = styled.div`
 /**
  * 공식 Spec: 상점 및 대부분의 시나리오에서 표시됩니다. 아이콘은 중앙에 96x96픽셀 기호가 포함된 총 192x192픽셀이어야 합니다.
  * 선 이외 모두 배경이 투명이면 기본 테마이외 왼쪽 사이드바에서 잘 보이지 않습니다. 이에 따라 선이 잘 표시되도록 뒷 배경을 흰색으로 추가하였습니다.
- * png 투명화 할 때 검은색 부분만 날리도록 하면 됩니다.
+ * png 투명화 할 때 green 부분만 날리도록 하면 됩니다.
  */
 export default function ColorIcon() {
   return (
--- ui-framework/fluentui-react-northstar/src/components/TeamsManifestBrandingIcon/OutlineIcon.tsx
+++ ui-framework/fluentui-react-northstar/src/components/TeamsManifestBrandingIcon/OutlineIcon.tsx
@@ -1,9 +1,4 @@
-import {
-  Provider,
-  teamsTheme,
-  teamsDarkTheme,
-  AppsIcon,
-} from "@fluentui/react-northstar";
+import { Provider, teamsTheme, AppsIcon } from "@fluentui/react-northstar";
 import styled from "styled-components";
 
 const Reset = styled.div`
@@ -18,9 +13,8 @@ const Reset = styled.div`
 
 /**
  * 공식 Spec: 앱이 사용 중일 때 주로 Teams의 왼쪽에 표시됩니다. 아이콘은 32x32픽셀이어야 하며 흰색 또는 투명해야 합니다.
- * 여기서 글씨가 검은색이면 기본 테마외 잘 보이지 않습니다. 흰색으로 하도록 합니다.
- * 테마에 따라 테스트를 위해서 backgroundColor를 teamsDarkTheme로 바꿔놓은 상태입니다.
- * png 투명화 할 때 검은색 부분만 날리도록 하면 됩니다.
+ * 여기서 선 색상이 검은색이면 기본 테마외 잘 보이지 않습니다. 선 색상을 흰색으로 하도록 합니다.
+ * png 투명화 할 때 green 부분만 날리도록 하면 됩니다.
  */
 export default function OutlineIcon() {
   return (
@@ -30,7 +24,7 @@ export default function OutlineIcon() {
           style={{
             width: "5rem",
             height: "5rem",
-            backgroundColor: teamsDarkTheme.siteVariables.bodyBackground,
+            backgroundColor: "green",
           }}
         >
           <Reset id="이 Element 노드 스크린샷 캡쳐">
