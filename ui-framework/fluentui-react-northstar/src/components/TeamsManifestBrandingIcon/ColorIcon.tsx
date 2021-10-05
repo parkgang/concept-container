64626d48f97907dc59f03ebba6ce78a7a93ea3bf
박경은 <ruddms936@naver.com>
2021-10-05 12:03:15 +0900

    up: 테마에 종속받지 않도록 잘 보이도록 수정

--- ui-framework/fluentui-react-northstar/src/components/TeamsManifestBrandingIcon/ColorIcon.tsx
+++ ui-framework/fluentui-react-northstar/src/components/TeamsManifestBrandingIcon/ColorIcon.tsx
@@ -1,21 +1,40 @@
-import { Provider, teamsTheme, AppsIcon } from "@fluentui/react-northstar";
+import {
+  Provider,
+  teamsTheme,
+  teamsDarkTheme,
+  AppsIcon,
+} from "@fluentui/react-northstar";
 import styled from "styled-components";
 
 const Reset = styled.div`
   width: 192px;
   height: 192px;
+  background-color: ${teamsDarkTheme.siteVariables.bodyBackground};
   /* 중앙 정렬 */
   display: flex;
   justify-content: center;
   align-items: center;
+  & > span {
+    width: 130px;
+    height: 130px;
+    border-radius: 5px;
+    background-color: white;
+    /* 중앙 정렬 */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
   & > span > svg {
     width: 96px;
     height: 96px;
+    color: black;
   }
 `;
 
 /**
- * 상점 및 대부분의 시나리오에서 표시됩니다. 아이콘은 중앙에 96x96픽셀 기호가 포함된 총 192x192픽셀이어야 합니다.
+ * 공식 Spec: 상점 및 대부분의 시나리오에서 표시됩니다. 아이콘은 중앙에 96x96픽셀 기호가 포함된 총 192x192픽셀이어야 합니다.
+ * 선 이외 모두 배경이 투명이면 기본 테마이외 왼쪽 사이드바에서 잘 보이지 않습니다. 이에 따라 선이 잘 표시되도록 뒷 배경을 흰색으로 추가하였습니다.
+ * png 투명화 할 때 검은색 부분만 날리도록 하면 됩니다.
  */
 export default function ColorIcon() {
   return (
--- ui-framework/fluentui-react-northstar/src/components/TeamsManifestBrandingIcon/OutlineIcon.tsx
+++ ui-framework/fluentui-react-northstar/src/components/TeamsManifestBrandingIcon/OutlineIcon.tsx
@@ -17,9 +17,10 @@ const Reset = styled.div`
 `;
 
 /**
- * 앱이 사용 중일 때 주로 Teams의 왼쪽에 표시됩니다. 아이콘은 32x32픽셀이어야 하며 흰색 또는 투명해야 합니다.
- * 여기서 글씨가 검은색이면 다크 테마시 표시가 안됩니다. 흰색으로 꼭 하도록 합니다.
- * 테마에 따라 테스트를 위해서 backgroundColor를 teamsDarkTheme로 바꿔놓은거긴 한데 png 투명화 할 때 검은색 부분만 검은색 날리도록 하면 편리합니다.
+ * 공식 Spec: 앱이 사용 중일 때 주로 Teams의 왼쪽에 표시됩니다. 아이콘은 32x32픽셀이어야 하며 흰색 또는 투명해야 합니다.
+ * 여기서 글씨가 검은색이면 기본 테마외 잘 보이지 않습니다. 흰색으로 하도록 합니다.
+ * 테마에 따라 테스트를 위해서 backgroundColor를 teamsDarkTheme로 바꿔놓은 상태입니다.
+ * png 투명화 할 때 검은색 부분만 날리도록 하면 됩니다.
  */
 export default function OutlineIcon() {
   return (
