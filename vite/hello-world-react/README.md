97d15e9f7e4bfde5a6a931efd28e40fd0097839a
박경은 <ruddms936@naver.com>
2022-03-22 19:30:27 +0900

    add: vite를 사용하여 빌드된 react 앱

---
+++ vite/hello-world-react/README.md
@@ -0,0 +1,9 @@
+# Overview
+
+1. Vite를 사용하여 빌드되는 React 샘플 코드 입니다.
+1. [해당 글](https://velog.io/@sehyunny/is-it-time-to-say-goodbye-to-webpack) 글을 참고했습니다.
+
+# Quick Start
+
+1. `npm i`
+1. `npm run dev`
---
+++ vite/hello-world-react/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <>
+      <div>Hello, World Vite</div>
+      <div>Time right now: {new Date().toTimeString()}</div>
+    </>
+  );
+}
