3e98e978fa3102e923d73918e8939ce5c5f7cd33
parkgang <ruddms936@naver.com>
2022-10-12 10:49:54 +0900

    feat: 멀티 스키마, 커넥션 예제 스크립트 추가

--- prisma/multi-schemas-and-multi-connections-with-sql-server/index.ts
+++ prisma/multi-schemas-and-multi-connections-with-sql-server/index.ts
@@ -1,1 +1,61 @@
-console.log("hello, world");
+import { PrismaClient as PrismaClientCatalog } from "./prisma/catalog/client";
+import { PrismaClient as PrismaClientTenant } from "./prisma/tenant/client";
+
+const catalogClient = new PrismaClientCatalog({
+  datasources: {
+    db: {
+      url: "sqlserver://localhost:1433;database=catalog;user=prisma_migrate;password=1234;encrypt=DANGER_PLAINTEXT",
+    },
+  },
+});
+const tenantAClient = new PrismaClientTenant({
+  datasources: {
+    db: {
+      url: "sqlserver://localhost:1433;database=tenant_a;user=prisma_migrate;password=1234;encrypt=DANGER_PLAINTEXT",
+    },
+  },
+});
+const tenantBClient = new PrismaClientTenant({
+  datasources: {
+    db: {
+      url: "sqlserver://localhost:1433;database=tenant_b;user=prisma_migrate;password=1234;encrypt=DANGER_PLAINTEXT",
+    },
+  },
+});
+
+async function main() {
+  const catalog = await catalogClient.table_a.create({
+    data: {
+      name: "catalog init",
+    },
+  });
+  console.log(catalog);
+
+  const tenantA = await tenantAClient.table_b.create({
+    data: {
+      title: "tenantA init",
+    },
+  });
+  console.log(tenantA);
+
+  const tenantB = await tenantBClient.table_b.create({
+    data: {
+      title: "tenantB init",
+    },
+  });
+  console.log(tenantB);
+}
+
+main()
+  .then(async () => {
+    await catalogClient.$disconnect();
+    await tenantAClient.$disconnect();
+    await tenantBClient.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await catalogClient.$disconnect();
+    await tenantAClient.$disconnect();
+    await tenantBClient.$disconnect();
+    process.exit(1);
+  });
