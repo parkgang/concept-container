b5a266052fbc06be8c50250300adbacc6b7aba34
parkgang <ruddms936@naver.com>
2022-11-09 04:26:00 +0000

    style: 주석 보강

--- trpc/trpc-with-iron-session/src/utils/trpc.ts
+++ trpc/trpc-with-iron-session/src/utils/trpc.ts
@@ -18,10 +18,13 @@ export const trpc = createTRPCNext<AppRouter>({
           url: `http://localhost:${process.env.PORT ?? 3000}/api/trpc`,
           headers() {
             if (ctx?.req) {
-              // SSR을 제대로 사용하려면 클라이언트의 헤더를 서버로 전달해야 합니다.
-              // 이는 SSR시 쿠키와 같은 것을 전달할 수 있도록 하기 위한 것입니다.
-              // 해당 작업을 진행하지 않으면 SSR시 쿠키가 전달되지 않음으로 SSR에서 쿠키 값으로 사용자 정보를 SSR 굽는 등 iron-session 처리를 할 수 없습니다.
-              // 햇갈리면 안되는 것은 해당 작업은 SSR시 처리를 위함이며 해당 코드가 없다고 해서 Client Side에서 호출되는 tRPC가 iron-session 처리를 못한다는 것이 아닙니다.
+              /**
+               * SSR을 제대로 사용하려면 클라이언트의 헤더를 서버로 전달해야 합니다.
+               * 이는 SSR시 쿠키와 같은 것을 전달할 수 있도록 하기 위한 것입니다.
+               * 해당 작업을 진행하지 않으면 SSR시 쿠키가 전달되지 않음으로 SSR에서 쿠키 값으로 사용자 정보를 SSR 굽는 등 iron-session 처리를 할 수 없습니다.
+               * 햇갈리면 안되는 것은 해당 작업은 SSR시 처리를 위함이며 해당 코드가 없다고 해서 Client Side에서 호출되는 tRPC가 iron-session 처리를 못한다는 것이 아닙니다.
+               * @see [가져온 코드 Origin](https://trpc.io/docs/v10/ssr)
+               */
               const { connection: _connection, ...headers } = ctx.req.headers;
               return {
                 ...headers,
