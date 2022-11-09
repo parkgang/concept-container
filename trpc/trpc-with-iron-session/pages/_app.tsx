bf3c2de26e0f975c8d314bb93e4a98b64aec6539
parkgang <ruddms936@naver.com>
2022-11-09 04:34:10 +0000

    feat: react-query 개발자 도구 붙임

--- trpc/trpc-with-iron-session/pages/_app.tsx
+++ trpc/trpc-with-iron-session/pages/_app.tsx
@@ -1,8 +1,14 @@
 import { AppProps } from "next/app";
 import { trpc } from "src/utils/trpc";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default trpc.withTRPC(MyApp);
