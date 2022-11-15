ecc373c0ebc98847abb052ffa3c19293333d2e7d
parkgang <ruddms936@naver.com>
2022-11-15 13:22:09 +0000

    chore: 주석 한글화

--- react-hook-form/official-get-started/pages/index.tsx
+++ react-hook-form/official-get-started/pages/index.tsx
@@ -14,17 +14,17 @@ export default function App() {
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
-  console.log(watch("example")); // watch input value by passing the name of it
+  console.log(watch("example")); // 이름을 전달하여 입력 값을 확인합니다.
 
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    /* "handleSubmit"은 "onSubmit"을 호출하기 전에 입력을 검증합니다 */
     <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
+      {/* "등록" 기능을 호출하여 후크에 입력을 등록합니다. */}
       <input defaultValue="test" {...register("example")} />
 
-      {/* include validation with required or other standard HTML validation rules */}
+      {/* 필수 또는 기타 표준 HTML 유효성 검사 규칙이 포함된 유효성 검사 포함 */}
       <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
+      {/* 필드 유효성 검사가 실패하면 오류가 반환됩니다. */}
       {errors.exampleRequired && <span>This field is required</span>}
 
       <input type="submit" />
