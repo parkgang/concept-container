17a49bab590f2216edbad9f21eb2b07c43ce5ca8
parkgang <ruddms936@naver.com>
2023-05-21 21:12:42 +0900

    refactor(node.js, dotenv)

--- node.js/js/study/index.js
+++
@@ -1,5 +0,0 @@
-// 9.1 Clear
-// 9.2 Clear
-// 9.3 Clear
-// 9.4 Clear: [이부분은 Skip함 추후, Express를 배우기 때문에 중요하지 않음](https://poiemaweb.com/nodejs-file-upload-example#express-framework)
-// 9.5 살펴만 봄, 추후 Express 프렘을 사용하면서 ".env" 환경변수 이용해보기
