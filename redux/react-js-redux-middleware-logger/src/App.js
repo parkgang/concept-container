f9fedee57512a5a4673e920e789850d895d1f6d4
parkgang <ruddms936@naver.com>
2023-05-21 23:49:48 +0900

    refactor(redux)

---
+++ redux/react-js-redux-middleware-logger/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import CounterContainer from './containers/CounterContainer';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <CounterContainer />
+    </>
+  );
+}
+
+export default App;
