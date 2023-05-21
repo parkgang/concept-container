5cc24f810dafcd9cda3f319cbf30f4cc52ab17a2
parkgang <ruddms936@naver.com>
2023-05-21 20:30:22 +0900

    refactor(fluentui-react-teams): 독립적인 디렉터리로 분리

--- fluentui-react-teams/fluentui-react-teams-play-ground/src/App.tsx
+++ fluentui-react-teams/fluentui-react-teams-play-ground/src/App.tsx
@@ -1,19 +1,29 @@
-import { useState } from "react";
 import { Provider as RTProvider, themeNames } from "@fluentui/react-teams";
+import { useState } from "react";
 
-import Welcome from "components/Communication/Welcome";
 import Error from "components/Communication/Error";
+import Welcome from "components/Communication/Welcome";
 
 export default function App() {
   const [rtTheme, setRtTheme] = useState<themeNames>(themeNames.Default);
 
   function handleRtTheme(theme: themeNames) {
-    return () => setRtTheme(theme);
+    return () => {
+      console.log("이벤트 핸들러 동작");
+      setRtTheme(theme);
+    };
   }
 
   return (
     <>
-      <header>
+      <header
+        style={{
+          // position: static 속성으로 인해 z-index이(가) 적용되지 않는 문제를 해결하기 위함
+          position: "relative",
+          // 하위 `<svg />` 가 `position: absolute` 속성을 가지고 있어서 이기기 위함
+          zIndex: 1,
+        }}
+      >
         <button onClick={handleRtTheme(themeNames.Default)}>Default</button>
         <button onClick={handleRtTheme(themeNames.Dark)}>Dark</button>
         <button onClick={handleRtTheme(themeNames.HighContrast)}>
