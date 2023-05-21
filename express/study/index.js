4c098384385dab35da77ad1fc42d326c60b4c95b
parkgang <ruddms936@naver.com>
2023-05-22 01:46:59 +0900

    chore: 누락된 파일 추가

---
+++ express/study/index.js
@@ -0,0 +1,6 @@
+// 10.1 [부터 대충 봄](https://poiemaweb.com/express-basics#34-response-method)
+// 10.2 Clear
+// 10.3 Clear: DBMS session 관리하는거 해보기
+// 10.4 Clear: mysql2 module 사용해야함
+// 10.5 Clear: 어떻게 되는지만 알고 넘어감
+// 10.6 Skip
