092c8a6ec86cac6992cbbfc6cfcf5ea76ec3db02
parkgang <ruddms936@naver.com>
2023-07-15 19:55:44 +0900

    feat(next.js-prefetch): 느린 이미지의 경우 어떻게 되는지 검증

--- next.js/next.js-prefetch/README.md
+++ next.js/next.js-prefetch/README.md
@@ -4,6 +4,14 @@
 
 `prefetch` 시 어떤 데이터를 받아오는지, `SSR` 인 경우는 어떻게 `prefetch` 가 이뤄지는지 등을 확인할 수 있습니다.
 
+## 확인된 것
+
+1. `prefetch` 으로 미리 불러오는 데이터는 `CSS` , `JS` 이다.
+   - `image` 의 경우 미리 불러오지 않는다.
+   - `CSS` 도 미리 불러오는지까지는 확인하지 못함
+1. `prefetch` 되었다고 해서 `SSR` 안되는 것이 아니다.
+   - `getServerSideProps` 은 `prefetch` 단계에서 수행되지 않는다.
+
 ## 시작
 
 ```shell
--- next.js/next.js-prefetch/pages/ssg-page.tsx
+++ next.js/next.js-prefetch/pages/ssg-page.tsx
@@ -1,17 +1,11 @@
-import Image from "next/image";
+import SlowImages from "@/components/SlowImages";
 
 export default function SsgPage() {
   return (
     <div>
       <h1>SSG Page!</h1>
       <p>ssg 페이지 입니다.</p>
-      <Image
-        src="/api/slow-image"
-        alt="아주 느리게 서빙되는 이미지"
-        width={473}
-        height={966}
-        style={{ border: "1px solid black" }}
-      />
+      <SlowImages />
     </div>
   );
 }
