a187af175a8e1a20ac69e8715975dc7d3ef2011c
parkgang <ruddms936@naver.com>
2023-07-15 19:23:11 +0900

    feat(next.js-prefetch): 의도적으로 이미지 파일 느리게 서빙하는 API 추가

--- next.js/next.js-prefetch/pages/ssg-page.tsx
+++ next.js/next.js-prefetch/pages/ssg-page.tsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
+
 export default function SsgPage() {
   return (
     <div>
       <h1>SSG Page!</h1>
       <p>ssg 페이지 입니다.</p>
+      <Image
+        src="/api/slow-image"
+        alt="아주 느리게 서빙되는 이미지"
+        width={473}
+        height={966}
+        style={{ border: "1px solid black" }}
+      />
     </div>
   );
 }
