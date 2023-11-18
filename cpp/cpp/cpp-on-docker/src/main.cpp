e7ee83cf0b9d2b21693511fdc86c61190a0cedb1
parkgang <ruddms936@naver.com>
2023-11-18 21:06:22 +0900

    feat: C++ 컴파일 결과 제외할 수 있도록

--- cpp/cpp/cpp-on-docker/src/main.cpp
+++ cpp/cpp/cpp-on-docker/src/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << endl;
 
     // C++ 표준 버전 출력
-    std::cout << "C++ Standard version: " << __cplusplus << std::endl;
+    cout << "C++ Standard version: " << __cplusplus << endl;
 
     return 0;
 }
