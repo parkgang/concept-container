d2ef6e9d49dba973ca2f49bff3ebc063996da585
parkgang <ruddms936@naver.com>
2024-02-23 08:13:43 +0900

    feat: pyautogui으로 스크린샷 캡처 예제

---
+++ python/pyautogui/pyautogui-screenshot/README.md
@@ -0,0 +1,8 @@
+`pyautogui` 를 이용해서 스크린샷을 찍는 코드입니다.
+
+## Start
+
+```shell
+pip3 install pyautogui
+python3 main.py
+```
---
+++ python/pyautogui/pyautogui-screenshot/main.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+# 스크린샷 찍을 영역: 시작점 (100, 200), 너비 500, 높이 300
+screenshot = pyautogui.screenshot(region=(100, 200, 500, 300))
+
+# 스크린샷 파일로 저장
+screenshot.save('screenshot.png')
